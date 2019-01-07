423a641a55fa9ad047816f1defd3ff6fa98bb9ef
Philippe Marion <contact@nivocart.com>
2019-01-07 15:57:24 +0000

    New translations article.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/blog/article.php
@@ -0,0 +1,63 @@
+<?php
+// Heading
+$_['heading_title']                = 'Article de blog';
+
+// Text
+$_['text_success']                 = 'Succès : Vous avez modifié des articles du Blog !';
+$_['text_browse']                  = 'Parcourir';
+$_['text_clear']                   = 'Effacer';
+$_['text_default']                 = 'Par défaut';
+$_['text_image_manager']           = 'Gestionnaire de fichiers';
+
+// Button
+$_['button_add_description']       = 'Ajouter Description';
+$_['button_add_articles']          = 'Ajouter un article';
+
+// Column
+$_['column_article_title']         = 'Titre de l’article';
+$_['column_author_name']           = 'Nom de l’auteur';
+$_['column_sort_order']            = 'Classement';
+$_['column_status']                = 'État ';
+$_['column_date_added']            = 'Date d’ajout';
+$_['column_action']                = 'Action';
+
+// Tab
+$_['tab_data']                     = 'Contenu';
+$_['tab_related']                  = 'Similaires';
+$_['tab_design']                   = 'Apparence';
+
+// Entry
+$_['entry_title']                  = 'Titre de l’article :<span class="help">titre de l’Article doit être unique.</span>';
+$_['entry_author_name']            = 'Nom de l’auteur :<span class="help">(Auto-complétion)</span>';
+$_['entry_meta_description']       = 'Meta Description :<span class="help">le nombre optimal de caractères est environ 155, y compris les espaces.<br />SEO importance : haute.</span>';
+$_['entry_meta_keyword']           = 'Mot clé Meta :<span class="help">généralement ignorés par les moteurs de recherche.<br />SEO importance : faible.</span>';
+$_['entry_description']            = 'Description :';
+$_['entry_allow_comment']          = 'Autoriser les commentaires pour cet article :';
+$_['entry_keyword']                = 'SEO Keyword:<span class="help">Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.</span>';
+$_['entry_image']                  = 'Image:<span class="help">Main article image.</span>';
+$_['entry_sort_order']             = 'Sort Order:';
+$_['entry_status']                 = 'Status:';
+$_['entry_category']               = 'Category:';
+$_['entry_manufacturer']           = 'Manufacturer:';
+$_['entry_product']                = 'Product:';
+$_['entry_productwise']            = '<span class="help"><b>Product Name:</b><br />(Autocomplete)</span>';
+$_['entry_store']                  = 'Store:';
+$_['entry_layout']                 = 'Layout Override:';
+$_['entry_additional_description'] = 'Additional Description';
+$_['entry_article_related_method'] = 'Article Related Products:<span class="help">This article is related to a single product, a product category, or a product manufacturer, select accordingly.</span>';
+$_['entry_blog_related_articles']  = 'Related Articles';
+$_['entry_category_wise']          = 'Category Wise';
+$_['entry_manufacturer_wise']      = 'Manufacturer Wise';
+$_['entry_product_wise']           = 'Product Wise';
+$_['entry_related_article_name']   = 'Article Name:<span class="help">(Autocomplete)</span>';
+
+// Error
+$_['error_warning']                = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']             = 'Warning: You do not have permission to modify article!';
+$_['error_title']                  = 'Article Title must be greater than 3 and less than 100 characters!';
+$_['error_title_found']            = 'Article Title already exists, article title must be unique!';
+$_['error_description']            = 'Description must be greater than 3 characters!';
+$_['error_author_name']            = 'Author name can not be blank!';
+$_['error_author_not_found_list']  = 'Author not found in our list!';
+$_['error_author_not_found']       = 'Sorry Author not found in our list, please insert valid author name!';
+$_['error_article_related']        = 'Warning: You can not delete article because currently it relate %s articles!';
