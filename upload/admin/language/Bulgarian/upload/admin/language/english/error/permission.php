5414783d4e7849c8029353dce72f3942cadcb80f
Philippe Marion <contact@nivocart.com>
2019-01-07 16:06:37 +0000

    New translations permission.php (Bulgarian)

---
+++ upload/admin/language/Bulgarian/upload/admin/language/english/error/permission.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title']   = 'Permission Denied!';
+
+// Text
+$_['text_permission'] = 'You do not have permission to access this page! Please refer to your System Administrator.';
