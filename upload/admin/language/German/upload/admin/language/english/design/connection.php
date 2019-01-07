3c632bf9846d500d4fdcccd5da7b384c88b23bbf
Philippe Marion <contact@nivocart.com>
2019-01-07 16:28:57 +0000

    New translations connection.php (German)

---
+++ upload/admin/language/German/upload/admin/language/english/design/connection.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']    = 'Links';
+
+// Text
+$_['text_success']     = 'Success: You have modified <b>Links</b> !';
+$_['text_default']     = 'Default';
+
+// Column
+$_['column_name']      = 'Link Name';
+$_['column_backend']   = 'Administration';
+$_['column_frontend']  = 'Store Front';
+$_['column_action']    = 'Action';
+
+// Entry
+$_['entry_name']       = 'Link Name:';
+$_['entry_backend']    = 'Administration (Admin):';
+$_['entry_frontend']   = 'Store Front (Catalog):';
+$_['entry_icon']       = 'Icon';
+$_['entry_title']      = 'Title';
+$_['entry_route']      = 'Address (Url)';
+
+// Button
+$_['button_module']    = 'Module Links';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify <b>Links</b> !';
+$_['error_name']       = 'Link Name must be between 3 and 64 characters!';
