0b5bc762747c82fb382e6e6670fc48754e5e9e45
Philippe Marion <contact@nivocart.com>
2019-01-07 15:48:00 +0000

    New translations category_list.php (Spanish)

---
+++ upload/catalog/language/Spanish/upload/catalog/language/english/product/category_list.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'All Categories';
+
+// Text
+$_['text_error']    = 'Category not found!';
+$_['text_empty']    = 'There are no products to display in this category.';
