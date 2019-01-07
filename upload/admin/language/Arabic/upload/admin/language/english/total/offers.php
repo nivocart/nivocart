9cc2740a7632bd8e567a7ccfb51beb5dd1f7a926
Philippe Marion <contact@nivocart.com>
2019-01-07 15:51:26 +0000

    New translations offers.php (Arabic)

---
+++ upload/admin/language/Arabic/upload/admin/language/english/total/offers.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Offers';
+
+// Text
+$_['text_total']       = 'Order Totals';
+$_['text_success']     = 'Success: You have modified <b>Offers</b> !';
+
+// Entry
+$_['entry_status']     = 'Status:';
+$_['entry_taxes']      = 'Calculate Taxes:';
+$_['entry_sort_order'] = 'Sort Order:';
+
+// Help
+$_['help_taxes']       = 'If Yes, corresponding product taxes will be applied to all offer discount values.';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify <b>Offers</b> !';
