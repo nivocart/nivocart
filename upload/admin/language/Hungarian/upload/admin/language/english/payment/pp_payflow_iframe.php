2cbf8fab025c9605b40f0fc62184571e90cec043
Philippe Marion <contact@nivocart.com>
2019-01-07 22:01:09 +0000

    New translations pp_payflow_iframe.php (Hungarian)

---
+++ upload/admin/language/Hungarian/upload/admin/language/english/payment/pp_payflow_iframe.php
@@ -0,0 +1,77 @@
+<?php
+// Heading
+$_['heading_title']                  = 'Paypal Payflow Pro iFrame';
+
+// Link
+$_['text_pp_payflow_iframe']         = '<a onclick="window.open(\'https://www.paypal.com/uk/\');"><img src="view/image/payment/paypal.png" alt="Paypal Payflow Pro iFrame" title="Paypal Payflow Pro iFrame" style="border:1px solid #EEEEEE;" /></a>';
+
+// Text
+$_['text_payment']                   = 'Payment';
+$_['text_success']                   = 'Success: You have modified payment <b>Paypal Payflow Pro iFrame</b> account details !';
+$_['text_transaction']               = 'Transaction';
+$_['text_sale']                      = 'Sale';
+$_['text_authorization']             = 'Authorization';
+$_['text_iframe']                    = 'Iframe';
+$_['text_redirect']                  = 'Redirect';
+$_['text_debug_clear_success']       = 'Success: You have successfully cleared your <b>Debug Log</b> !';
+
+// Tab
+$_['tab_api']                        = 'API Details';
+$_['tab_order_status']               = 'Order Status';
+$_['tab_debug_log']                  = 'Debug Log';
+
+// Entry
+$_['entry_partner']                  = 'Partner:';
+$_['entry_vendor']                   = 'Vendor:';
+$_['entry_username']                 = 'User:';
+$_['entry_password']                 = 'Password:';
+$_['entry_cancel_url']               = 'Cancel URL:';
+$_['entry_error_url']                = 'Error URL:';
+$_['entry_return_url']               = 'Return URL:';
+$_['entry_silent_post_url']          = 'Silent POST URL (Public URL to send your transaction results to):';
+
+$_['entry_test']                     = 'Test mode:';
+$_['entry_debug']                    = 'Debug mode:';
+$_['entry_transaction_method']       = 'Transaction method:';
+$_['entry_checkout_method']          = 'Checkout method:';
+$_['entry_timeout']                  = 'Timeout:';
+$_['entry_order_status']             = 'Order status:';
+$_['entry_total']                    = 'Total:';
+$_['entry_total_max']                = 'Total maximum:';
+$_['entry_geo_zone']                 = 'Geo zone:';
+$_['entry_status']                   = 'Status:';
+$_['entry_sort_order']               = 'Sort order:';
+
+$_['entry_canceled_reversal_status'] = 'Canceled Reversal Status:';
+$_['entry_completed_status']         = 'Completed Status:';
+$_['entry_denied_status']            = 'Denied Status:';
+$_['entry_expired_status']           = 'Expired Status:';
+$_['entry_failed_status']            = 'Failed Status:';
+$_['entry_pending_status']           = 'Pending Status:';
+$_['entry_processed_status']         = 'Processed Status:';
+$_['entry_refunded_status']          = 'Refunded Status:';
+$_['entry_reversed_status']          = 'Reversed Status:';
+$_['entry_voided_status']            = 'Voided Status:';
+
+// Help
+$_['help_partner']                   = 'The ID provided to you by the authorised PayPal Reseller who registered you for the Payflow SDK. If you purchased your account directly from PayPal, use "paypal".';
+$_['help_vendor']                    = 'Your merchant login ID that you created when you registered for the Website Payments Pro account.';
+$_['help_username']                  = 'If you set up one or more additional users on the account, this value is the ID of the user authorised to process transactions. If, however, you have not set up additional users on the account, USER has the same value as VENDOR.';
+$_['help_password']                  = 'The 6 to 32 character password that you defined while registering for the account.';
+$_['help_test']                      = 'Use the live or testing (sandbox) gateway server to process transactions?';
+$_['help_debug']                     = 'Logs additional information.';
+$_['help_transaction_method']        = 'Sale will charge customer immediately. Authorization will put funds on hold for future capture.';
+$_['help_checkout_method']           = 'Please use Redirect method if do not have SSL installed or if you do not have Pay with PayPal option disabled on your hosted payment page.';
+$_['help_timeout']                   = 'The maximum duration in secondes a request is attempted.';
+$_['help_total']                     = 'The checkout total the order must reach before this payment method becomes <b>active</b>.';
+$_['help_total_max']                 = 'The maximum checkout total the order must reach before this payment method becomes <b>inactive</b>.<br />Leave empty for no maximum.';
+
+// Button
+$_['button_pp_manager']              = 'PayPal Manager';
+
+// Error
+$_['error_permission']               = 'Warning: You do not have permission to modify payment <b>Paypal Payflow Pro iFrame</b> !';
+$_['error_partner']                  = 'Partner required!';
+$_['error_vendor']                   = 'Vendor required!';
+$_['error_username']                 = 'User required!';
+$_['error_password']                 = 'Password required!';
