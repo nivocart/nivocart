f1f61bcc354d53f9302d203b5b2aca59e192c520
Philippe Marion <contact@nivocart.com>
2019-01-07 16:15:49 +0000

    New translations customer_online.php (Czech)

---
+++ upload/admin/language/Czech/upload/admin/language/english/report/customer_online.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']     = 'Customers Online Report';
+
+// Text
+$_['text_guest']        = 'Guest';
+$_['text_tracking']     = 'You must enable "Customers Online" in your Settings to view this report.';
+$_['text_tooltip']      = 'Active Customers in the last 15 minutes.';
+
+// Column
+$_['column_ip']         = 'IP';
+$_['column_customer']   = 'Customer';
+$_['column_url']        = 'Last Page Visited';
+$_['column_referer']    = 'Referer';
+$_['column_user_agent'] = 'System';
+$_['column_date_added'] = 'Last Active';
+$_['column_action']     = 'Action';
