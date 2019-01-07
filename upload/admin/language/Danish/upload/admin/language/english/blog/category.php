ed0daaeb424f98cd128847580ccd677f939c601a
Philippe Marion <contact@nivocart.com>
2019-01-07 16:23:47 +0000

    New translations category.php (Danish)

---
+++ upload/admin/language/Danish/upload/admin/language/english/blog/category.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']          = 'Blog Category';
+
+// Text
+$_['text_success']           = 'Success: You have modified Blog Categories!';
+$_['text_default']           = 'Default';
+$_['text_image_manager']     = 'File Manager';
+$_['text_browse']            = 'Browse Files';
+$_['text_clear']             = 'Clear Image';
+
+// Column
+$_['column_name']            = 'Category Name';
+$_['column_sort_order']      = 'Sort Order';
+$_['column_status']          = 'Status';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Category Name:';
+$_['entry_meta_description'] = 'Meta Tag Description:<span class="help">The optimal number of characters is around 155, including spaces.<br />SEO importance: High.</span>';
+$_['entry_meta_keyword']     = 'Meta Tag Keywords:<span class="help">Usually ignored by search engines.<br />SEO importance: Low.</span>';
+$_['entry_description']      = 'Description:';
+$_['entry_parent']           = 'Parent Category:';
+$_['entry_store']            = 'Stores:';
+$_['entry_keyword']          = 'SEO Keyword:<span class="help">Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.</span>';
+$_['entry_image']            = 'Image:';
+$_['entry_article_limit']    = 'Article limit per page:<span class="help">Number of articles displayed on page in this category.</span>';
+$_['entry_sort_order']       = 'Sort Order:';
+$_['entry_status']           = 'Status:';
+$_['entry_layout']           = 'Layout Override:';
+
+// Error
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify categories!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_article']          = 'Warning: This Category cannot be deleted as it is currently assigned to %s articles!';
