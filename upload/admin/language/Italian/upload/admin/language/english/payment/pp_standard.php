2be3d29a73353f03e97dcd68b7e83f34952192be
Philippe Marion <contact@nivocart.com>
2019-01-07 22:09:50 +0000

    New translations pp_standard.php (Italian)

---
+++ upload/admin/language/Italian/upload/admin/language/english/payment/pp_standard.php
@@ -0,0 +1,47 @@
+<?php
+// Heading
+$_['heading_title']                  = 'PayPal Standard';
+
+// Link
+$_['text_pp_standard']               = '<a onclick="window.open(\'https://www.paypal.com/\');"><img src="view/image/payment/paypal.png" alt="PayPal Payments Standard" title="PayPal Payments Standard" style="border:1px solid #EEEEEE;" /></a>';
+
+// Text
+$_['text_payment']                   = 'Payment';
+$_['text_success']                   = 'Success: You have modified <b>PayPal Standard</b> account details!';
+$_['text_authorization']             = 'Authorization';
+$_['text_sale']                      = 'Sale';
+
+// Entry
+$_['entry_email']                    = 'Email';
+$_['entry_test']                     = 'Sandbox Mode';
+$_['entry_transaction']              = 'Transaction Method';
+$_['entry_debug']                    = 'Debug Mode';
+$_['entry_total']                    = 'Total';
+$_['entry_total_max']                = 'Total Maximum';
+$_['entry_canceled_reversal_status'] = 'Canceled Reversal Status';
+$_['entry_completed_status']         = 'Completed Status';
+$_['entry_denied_status']            = 'Denied Status';
+$_['entry_expired_status']           = 'Expired Status';
+$_['entry_failed_status']            = 'Failed Status';
+$_['entry_pending_status']           = 'Pending Status';
+$_['entry_processed_status']         = 'Processed Status';
+$_['entry_refunded_status']          = 'Refunded Status';
+$_['entry_reversed_status']          = 'Reversed Status';
+$_['entry_voided_status']            = 'Voided Status';
+$_['entry_geo_zone']                 = 'Geo Zone';
+$_['entry_status']                   = 'Status';
+$_['entry_sort_order']               = 'Sort Order';
+
+// Tab
+$_['tab_order_status']               = 'Order Status';
+
+// Help
+$_['help_test']                      = 'Use the live or testing (sandbox) gateway server to process transactions?';
+$_['help_debug']                     = 'Logs additional information to the system log.';
+$_['help_total']                     = 'The checkout total the order must reach before this payment method becomes <b>active</b>.';
+$_['help_total_max']                 = 'The maximum checkout total the order must reach before this payment method becomes <b>inactive</b>.<br />Leave empty for no maximum.';
+$_['help_transaction']               = 'Sale will charge customer immediately. Authorization will put funds on hold for future capture.';
+
+// Error
+$_['error_permission']               = 'Warning: You do not have permission to modify <b>PayPal Standard</b>!';
+$_['error_email']                    = 'Email is required!';
