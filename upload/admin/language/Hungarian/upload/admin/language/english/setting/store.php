688e2e211c0f24c4cda2971ac154d7eabbe81828
Philippe Marion <contact@nivocart.com>
2019-01-07 22:00:04 +0000

    New translations store.php (Hungarian)

---
+++ upload/admin/language/Hungarian/upload/admin/language/english/setting/store.php
@@ -0,0 +1,153 @@
+<?php
+// Heading
+$_['heading_title']                = 'Settings';
+
+// Text
+$_['text_success']                 = 'Success: You have modified <b>Stores</b> !';
+$_['text_tax']                     = 'Taxes';
+$_['text_account']                 = 'Account';
+$_['text_checkout']                = 'Checkout';
+$_['text_one_page']                = 'One Page Checkout';
+$_['text_express']                 = 'Express Checkout';
+$_['text_stock']                   = 'Stock';
+$_['text_supplier']                = 'Supplier';
+$_['text_colorbox']                = 'Colorbox';
+$_['text_fancybox']                = 'FancyBox';
+$_['text_magnific']                = 'Magnific';
+$_['text_swipebox']                = 'SwipeBox';
+$_['text_zoomlens']                = 'Zoom Lens';
+$_['text_image_resize']            = 'Image Dimensions';
+$_['text_image_manager']           = 'File Manager';
+$_['text_browse']                  = 'Browse';
+$_['text_clear']                   = 'Clear';
+$_['text_shipping']                = 'Shipping Address';
+$_['text_payment']                 = 'Payment Address';
+
+// Column
+$_['column_name']                  = 'Store Name';
+$_['column_template']              = 'Template';
+$_['column_url']                   = 'URL';
+$_['column_action']                = 'Action';
+
+// Info
+$_['info_one_page']                = 'New customer accounts will always be created, but not automatically approved. Guest Checkout is not possible.';
+$_['info_express']                 = 'Express Checkout: Please note that customer accounts will always be created and systematically approved, therefore Guest Checkout will no longer be possible.';
+
+// Entry
+$_['entry_url']                    = 'Store URL:';
+$_['entry_ssl']                    = 'Store SSL URL:';
+$_['entry_name']                   = 'Store Name:';
+$_['entry_owner']                  = 'Store Owner:';
+$_['entry_address']                = 'Store Address:';
+$_['entry_email']                  = 'Store Email:';
+$_['entry_email_noreply'] = 'No reply Email:';
+$_['entry_telephone']              = 'Telephone:';
+$_['entry_fax']                    = 'Fax:';
+$_['entry_title']                  = 'Multi-store Title:';
+$_['entry_meta_description']       = 'Meta Tag Description:';
+$_['entry_layout']                 = 'Default Layout:';
+$_['entry_template']               = 'Template:';
+$_['entry_country']                = 'Country:';
+$_['entry_zone']                   = 'Region / State:';
+$_['entry_language']               = 'Language:';
+$_['entry_currency']               = 'Currency:';
+$_['entry_cart_weight']            = 'Display Weight on Cart Page:';
+$_['entry_guest_checkout']         = 'Guest Checkout:';
+$_['entry_one_page_checkout']      = 'One Page Checkout (<b>Master</b>):';
+$_['entry_express_checkout']       = 'Express Checkout (<b>Master</b>):';
+$_['entry_checkout']               = 'Checkout Terms:';
+$_['entry_order_status']           = 'Order Status:';
+$_['entry_tax']                    = 'Display Prices With Tax:';
+$_['entry_tax_default']            = 'Use Store Tax Address:';
+$_['entry_tax_customer']           = 'Use Customer Tax Address:';
+$_['entry_customer_group']         = 'Customer Group:';
+$_['entry_customer_group_display'] = 'Customer Groups:';
+$_['entry_customer_price']         = 'Login Display Prices:';
+$_['entry_account']                = 'Account Terms:';
+$_['entry_stock_display']          = 'Display Stock:';
+$_['entry_stock_checkout']         = 'Stock Checkout:';
+$_['entry_supplier_group']         = 'Supplier Group:';
+$_['entry_catalog_limit']          = 'Default Items Per Page (Catalog):';
+$_['entry_lightbox']               = 'Product Image Viewer:';
+$_['entry_logo']                   = 'Store Logo:';
+$_['entry_icon']                   = 'Store Icon:';
+$_['entry_image_category']         = 'Category Image Size:';
+$_['entry_image_thumb']            = 'Product Image Thumb Size:';
+$_['entry_image_popup']            = 'Product Image Popup Size:';
+$_['entry_image_product']          = 'Product Image List Size:';
+$_['entry_image_additional']       = 'Additional Product Image Size:';
+$_['entry_image_brand']            = 'Manufacturer Image Size:';
+$_['entry_image_related']          = 'Related Product Image Size:';
+$_['entry_image_compare']          = 'Compare Image Size:';
+$_['entry_image_wishlist']         = 'Wish List Image Size:';
+$_['entry_image_newsthumb']        = 'News Image Thumb Size:';
+$_['entry_image_newspopup']        = 'News Image Popup Size:';
+$_['entry_image_cart']             = 'Cart Image Size:';
+$_['entry_secure']                 = 'Use SSL:';
+
+// Button
+$_['button_themes']                = 'Themes';
+
+// Help
+$_['help_url']                     = 'Include the full URL to your store. Make sure to add \'/\' at the end. Example: http://www.yourdomain.com/path/<br /><br />Don\'t use directories to create a new store. You should always point another domain or sub domain to your hosting.';
+$_['help_ssl']                     = 'SSL URL to your store. Make sure to add \'/\' at the end. Example: https://www.yourdomain.com/path/<br /><br />Don\'t use directories to create a new store. You should always point another domain or sub domain to your hosting.';
+$_['help_cart_weight']             = 'Show the cart weight on the cart page.';
+$_['help_guest_checkout']          = 'Allow customers to checkout without creating an account. This will not be available when a downloadable product is in the shopping cart.';
+$_['help_one_page_checkout']       = 'Enable One Page Checkout. Overrides the default Checkout System.';
+$_['help_express_checkout']        = 'Enable Express Checkout. Overrides the default Checkout System.';
+$_['help_checkout']                = 'Forces people to agree to terms before an a customer can checkout.';
+$_['help_order_status']            = 'Set the default order status when an order is processed.';
+$_['help_tax_default']             = 'Use the store address to calculate taxes if no one is logged in. You can choose to use the store address for the customers shipping or payment address.';
+$_['help_tax_customer']            = 'Use the customers default address when they login to calculate taxes. You can choose to use the default address for the customers shipping or payment address.';
+$_['help_customer_group']          = 'Default customer group.';
+$_['help_customer_group_display']  = 'Display customer groups that new customers can select to use such as wholesale and business when signing up.';
+$_['help_customer_price']          = 'Only show prices when a customer is logged in.';
+$_['help_account']                 = 'Forces people to agree to terms before an account can be created.';
+$_['help_stock_display']           = 'Display stock quantity on the product page.';
+$_['help_stock_checkout']          = 'Allow customers to still checkout if the products they are ordering are not in stock.';
+$_['help_supplier_group']          = 'Default supplier group.';
+$_['help_catalog_limit']           = 'Determines how many catalog items are shown per page (products, categories, etc)';
+$_['help_lightbox']                = 'If "Zoom Lens" is selected, the Product Image size will be set at 230 x 230px.';
+$_['help_icon']                    = 'The icon should be a PNG that is 16px x 16px.';
+$_['help_image_category']          = 'Category image on Category pages.';
+$_['help_image_thumb']             = 'Product Main image on Product pages.';
+$_['help_image_popup']             = 'Product Popup image on Product pages.';
+$_['help_image_product']           = 'Product images on Category, Search, Special, Reviews and other Product pages.';
+$_['help_image_additional']        = 'Product additional image(s) on Product pages.';
+$_['help_image_brand']             = 'Manufacturer image on Manufacturer pages.';
+$_['help_image_related']           = 'Related Product images on Product pages.';
+$_['help_image_compare']           = 'Product image on product Compare pages.';
+$_['help_image_wishlist']          = 'Product image on customer Wish List pages.';
+$_['help_image_newsthumb']         = 'News Main image on news pages.';
+$_['help_image_newspopup']         = 'News Popup image on news pages.';
+$_['help_image_cart']              = 'Product image on Cart pages.';
+$_['help_secure']                  = 'To use SSL check with your host if a SSL certificate is installed.';
+
+// Error
+$_['error_warning']                = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']             = 'Warning: You do not have permission to modify <b>Stores</b> !';
+$_['error_name']                   = 'Store Name must be between 3 and 32 characters!';
+$_['error_owner']                  = 'Store Owner must be between 3 and 64 characters!';
+$_['error_address']                = 'Store Address must be between 10 and 256 characters!';
+$_['error_email']                  = 'Email Address does not appear to be valid!';
+$_['error_email_noreply'] = 'Email Address does not appear to be valid!';
+$_['error_telephone']              = 'Telephone must be between 3 and 32 characters!';
+$_['error_url']                    = 'Store URL required!';
+$_['error_title']                  = 'Title must be between 3 and 32 characters!';
+$_['error_multiple_checkout']      = 'You can only use one type of Checkout!';
+$_['error_customer_group_display'] = 'You must include the default customer group if you are going to use this feature!';
+$_['error_limit']                  = 'Limit required!';
+$_['error_image_category']         = 'Category List Size dimensions required!';
+$_['error_image_thumb']            = 'Product Image Thumb Size dimensions required!';
+$_['error_image_popup']            = 'Product Image Popup Size dimensions required!';
+$_['error_image_product']          = 'Product List Size dimensions required!';
+$_['error_image_additional']       = 'Additional Product Image Size dimensions required!';
+$_['error_image_brand']            = 'Manufacturer Image Size dimensions required!';
+$_['error_image_related']          = 'Related Product Image Size dimensions required!';
+$_['error_image_compare']          = 'Compare Image Size dimensions required!';
+$_['error_image_wishlist']         = 'Wish List Image Size dimensions required!';
+$_['error_image_newsthumb']        = 'News Image Thumb Size dimensions required!';
+$_['error_image_newspopup']        = 'News Image Popup Size dimensions required!';
+$_['error_image_cart']             = 'Cart Image Size dimensions required!';
+$_['error_default']                = 'Warning: You can not delete your default store!';
+$_['error_store']                  = 'Warning: This Store cannot be deleted as it is currently assigned to %s orders!';
