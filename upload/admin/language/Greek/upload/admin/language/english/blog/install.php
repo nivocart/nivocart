656281b185b9a567780afc69a6513d4c28018b15
Philippe Marion <contact@nivocart.com>
2019-01-07 21:37:41 +0000

    New translations install.php (Greek)

---
+++ upload/admin/language/Greek/upload/admin/language/english/blog/install.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']        = 'Install';
+
+// Text
+$_['text_install_message'] = 'Missing Database Tables!';
+$_['text_upgrade']         = 'Go to "modification/blog" to Install';
+
+// Error
+$_['error_database']       = 'Database Not Found!';
