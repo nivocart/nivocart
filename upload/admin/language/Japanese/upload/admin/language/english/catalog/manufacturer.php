0d974010ed2bdafe6538011c763f7952938826c2
Philippe Marion <contact@nivocart.com>
2019-01-07 22:12:14 +0000

    New translations manufacturer.php (Japanese)

---
+++ upload/admin/language/Japanese/upload/admin/language/english/catalog/manufacturer.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']      = 'Manufacturer';
+
+// Text
+$_['text_success']       = 'Success: You have modified <b>Manufacturers</b> !';
+$_['text_default']       = 'Default';
+$_['text_image_manager'] = 'File Manager';
+$_['text_browse']        = 'Browse';
+$_['text_clear']         = 'Clear';
+
+// Column
+$_['column_id']          = 'ID';
+$_['column_image']       = 'Image';
+$_['column_name']        = 'Manufacturer\'s Name';
+$_['column_sort_order']  = 'Sort Order';
+$_['column_status']      = 'Status';
+$_['column_action']      = 'Action';
+
+// Entry
+$_['entry_name']         = 'Manufacturer\'s Name:';
+$_['entry_description']  = 'Manufacturer\'s Description:';
+$_['entry_store']        = 'Stores:';
+$_['entry_keyword']      = 'SEO Keyword:';
+$_['entry_image']        = 'Image:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sort Order:';
+
+// Help
+$_['help_keyword']       = 'Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify <b>Manufacturers</b> !';
+$_['error_warning']      = 'Warning: Please check the form carefully for errors!';
+$_['error_name']         = 'Manufacturer\'s Name must be between 2 and 128 characters!';
+$_['error_image']        = 'Image must be a valid image format!';
+$_['error_product']      = 'Warning: This manufacturer cannot be deleted as it is currently assigned to %s products!';
