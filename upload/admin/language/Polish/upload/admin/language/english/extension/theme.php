f5faf5991cc0ce46e1daa41e9fa514b2e5640cfa
Philippe Marion <contact@nivocart.com>
2019-01-08 02:03:28 +0000

    New translations theme.php (Polish)

---
+++ upload/admin/language/Polish/upload/admin/language/english/extension/theme.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Themes';
+
+// Text
+$_['text_install']     = 'Install';
+$_['text_uninstall']   = 'Uninstall';
+$_['text_is_active']   = '<b>(Active)</b>';
+
+// Column
+$_['column_name']      = 'Theme Name';
+$_['column_action']    = 'Action';
+
+// Button
+$_['button_settings']  = 'Settings';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify <b>Themes</b> !';
