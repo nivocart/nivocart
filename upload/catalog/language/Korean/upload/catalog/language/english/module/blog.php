40e2e4c948355a260884324d7f99a4118a3c5082
Philippe Marion <contact@nivocart.com>
2019-01-08 01:39:45 +0000

    New translations blog.php (Korean)

---
+++ upload/catalog/language/Korean/upload/catalog/language/english/module/blog.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']           = 'Blog';
+
+// Text
+$_['text_comment']            = 'Comment';
+$_['text_comments']           = 'Comments';
+$_['text_date_format']        = 'F jS, Y';
+$_['text_not_found']          = 'No Result!';
+
+// Button
+$_['button_continue_reading'] = 'Continue Reading';
