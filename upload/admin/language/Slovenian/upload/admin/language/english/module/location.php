8dc10440ae42f6e2379436b33281338271b79d30
Philippe Marion <contact@nivocart.com>
2019-01-08 14:26:53 +0000

    New translations location.php (Slovenian)

---
+++ upload/admin/language/Slovenian/upload/admin/language/english/module/location.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']         = 'Location';
+
+// Text
+$_['text_module']           = 'Modules';
+$_['text_success']          = 'Success: You have modified module <b>Location</b> !';
+$_['text_manage_locations'] = 'Add, Edit or View, store locations.';
+
+// Entry
+$_['entry_theme']           = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']           = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+
+$_['entry_limit']           = 'Limit';
+$_['entry_image']           = 'Image (W x H)';
+$_['entry_layout']          = 'Layout';
+$_['entry_position']        = 'Position';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Button
+$_['button_manager']        = 'Location Manager';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify module <b>Location</b> !';
+$_['error_image']           = 'Width &amp; Height dimensions required!';
