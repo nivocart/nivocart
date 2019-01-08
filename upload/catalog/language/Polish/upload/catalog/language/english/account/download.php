fadaea9c324cfbaecde891acc1818e2395f85c2d
Philippe Marion <contact@nivocart.com>
2019-01-08 14:07:18 +0000

    New translations download.php (Polish)

---
+++ upload/catalog/language/Polish/upload/catalog/language/english/account/download.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']   = 'Account Downloads';
+
+// Text
+$_['text_account']    = 'Account';
+$_['text_downloads']  = 'Downloads';
+$_['text_order']      = 'Order ID:';
+$_['text_date_added'] = 'Date Added:';
+$_['text_name']       = 'Name:';
+$_['text_remaining']  = 'Remaining:';
+$_['text_size']       = 'Size:';
+$_['text_empty']      = 'You do not have any downloadable orders at this time.';
