16e22c681fcb8df9e4df6fe965c083a7d03f57c7
Philippe Marion <contact@nivocart.com>
2019-01-08 01:51:47 +0000

    New translations menu_vertical.php (Lithuanian)

---
+++ upload/admin/language/Lithuanian/upload/admin/language/english/module/menu_vertical.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']         = 'Menu Vertical';
+
+// Text
+$_['text_module']           = 'Modules';
+$_['text_success']          = 'Success: You have modified module <b>Menu Vertical</b> !';
+$_['text_yes']              = 'Yes';
+$_['text_no']               = 'No';
+$_['text_ltr']              = 'Left to Right';
+$_['text_rtl']              = 'Right to Left';
+$_['text_manage_menu']      = 'Add, Edit or View, Menus and Menu items.';
+$_['text_no_menu']          = 'No menu !';
+$_['text_menus']            = 'Menus';
+
+// Entry
+$_['entry_theme']           = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']           = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+
+$_['entry_menu']            = 'Menu';
+$_['entry_layout']          = 'Layout';
+$_['entry_position']        = 'Position';
+$_['entry_direction']       = 'Direction';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Button
+$_['button_manager']        = 'Menus Manager';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify module <b>Menu Vertical</b> !';
+$_['error_menu']            = 'Warning: You must select a Menu!';
