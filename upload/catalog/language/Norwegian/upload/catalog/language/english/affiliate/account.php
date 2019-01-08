c1673bf8ac6340dc8424fce8eab1c7a862f2aabd
Philippe Marion <contact@nivocart.com>
2019-01-08 02:01:42 +0000

    New translations account.php (Norwegian)

---
+++ upload/catalog/language/Norwegian/upload/catalog/language/english/affiliate/account.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']        = 'My Affiliate Account';
+
+// Text
+$_['text_account']         = 'Account';
+$_['text_my_account']      = 'My affiliate account';
+$_['text_my_tracking']     = 'My tracking information';
+$_['text_my_transactions'] = 'My transactions';
+$_['text_edit']            = 'Edit your account information';
+$_['text_password']        = 'Change your password';
+$_['text_payment']         = 'Change your payment preferences';
+$_['text_product']         = 'Edit your product portfolio';
+$_['text_tracking']        = 'Custom product tracking code';
+$_['text_transaction']     = 'View your transaction history';
