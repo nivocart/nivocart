b178dfc990efa8ff1af17643a17b4df5ac2eb581
Philippe Marion <contact@nivocart.com>
2019-01-08 14:10:07 +0000

    New translations pp_pro_pf.php (Portuguese)

---
+++ upload/admin/language/Portuguese/upload/admin/language/english/payment/pp_pro_pf.php
@@ -0,0 +1,43 @@
+<?php
+// Heading
+$_['heading_title']      = 'PayPal Payments Pro Payflow';
+
+// Link
+$_['text_pp_pro_pf']     = '<a onclick="window.open(\'https://www.paypal.com/\');"><img src="view/image/payment/paypal.png" alt="PayPal Payments Pro Payflow Edition" title="PayPal Payments Pro Payflow Edition" style="border:1px solid #EEEEEE;" /></a>';
+
+// Text
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified <b>PayPal Payments Pro Payflow</b> account details !';
+$_['text_authorization'] = 'Authorization';
+$_['text_sale']          = 'Sale';
+
+// Entry
+$_['entry_vendor']       = 'Vendor';
+$_['entry_user']         = 'User';
+$_['entry_password']     = 'Password';
+$_['entry_partner']      = 'Partner';
+$_['entry_test']         = 'Test Mode';
+$_['entry_transaction']  = 'Transaction Method';
+$_['entry_total']        = 'Total';
+$_['entry_total_max']    = 'Total Maximum';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_vendor']        = 'Your merchant login ID that you created when you registered for the Website Payments Pro account';
+$_['help_user']          = 'If you set up one or more additional users on the account, this value is the ID of the user authorised to process transactions. If, however, you have not set up additional users on the account, USER has the same value as VENDOR';
+$_['help_password']      = 'The 6 to 32 character password that you defined while registering for the account';
+$_['help_partner']       = 'The ID provided to you by the authorised PayPal Reseller who registered you for the Payflow SDK. If you purchased your account directly from PayPal, use the PayPal Pro instead';
+$_['help_test']          = 'Use the live or testing (sandbox) gateway server to process transactions?';
+$_['help_transaction']   = 'Sale will charge customer immediately. Authorization will put funds on hold for future capture.';
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes <b>active</b>';
+$_['help_total_max']     = 'The maximum checkout total the order must reach before this payment method becomes <b>inactive</b>.<br />Leave empty for no maximum.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify <b>PayPal Payments Pro Payflow</b> !';
+$_['error_vendor']       = 'Vendor Required!';
+$_['error_user']         = 'User Required!';
+$_['error_password']     = 'Password Required!';
+$_['error_partner']      = 'Partner Required!';
