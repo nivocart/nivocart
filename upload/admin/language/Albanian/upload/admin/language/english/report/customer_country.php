e10135c78f163fa047def9bcc62f45511e2d68c3
Philippe Marion <contact@nivocart.com>
2019-01-08 14:35:18 +0000

    New translations customer_country.php (Albanian)

---
+++ upload/admin/language/Albanian/upload/admin/language/english/report/customer_country.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']     = 'Customer Countries Report';
+
+// Column
+$_['column_country_id'] = 'ID';
+$_['column_name']       = 'Country Name';
+$_['column_customers']  = 'Customers';
+$_['column_ratio']      = 'Ratio';
+$_['column_graph']      = 'Graph';
