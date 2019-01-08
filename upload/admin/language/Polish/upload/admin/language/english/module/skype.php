bf30b6b2763134c0ff9abf1d4de65bc02bf8115e
Philippe Marion <contact@nivocart.com>
2019-01-08 14:09:34 +0000

    New translations skype.php (Polish)

---
+++ upload/admin/language/Polish/upload/admin/language/english/module/skype.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']         = 'Skype';
+
+// Text
+$_['text_module']           = 'Modules';
+$_['text_success']          = 'Success: You have modified module <b>Skype</b> !';
+$_['text_call']             = 'Call';
+$_['text_chat']             = 'Chat';
+
+// Entry
+$_['entry_theme']           = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']           = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+$_['entry_skypename']       = 'Skype Name:';
+
+$_['entry_mode']            = 'Mode';
+$_['entry_layout']          = 'Layout';
+$_['entry_position']        = 'Position';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify module <b>Skype</b> !';
+$_['error_skypename']       = 'Warning: A Skype Name is Required!';
