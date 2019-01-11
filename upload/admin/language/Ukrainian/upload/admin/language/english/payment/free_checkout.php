4b0d0b78d3baacca6fc4b686c8c998c30d66026e
Philippe Marion <contact@nivocart.com>
2019-01-11 10:22:42 +0000

    New translations free_checkout.php (Ukrainian)

---
+++ upload/admin/language/Ukrainian/upload/admin/language/english/payment/free_checkout.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']      = 'Free Checkout';
+
+// Link
+$_['text_free_checkout'] = '';
+
+// Text
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified payment <b>Free Checkout</b> !';
+
+// Entry
+$_['entry_total']        = 'Total';
+$_['entry_total_max']    = 'Total Maximum';
+$_['entry_order_status'] = 'Order Status:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sort Order:';
+
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes <b>active</b>.';
+$_['help_total_max']     = 'The maximum checkout total the order must reach before this payment method becomes <b>inactive</b>.<br />Leave empty for no maximum.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment <b>Free Checkout</b> !';
