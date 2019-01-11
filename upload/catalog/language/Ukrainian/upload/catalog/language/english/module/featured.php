35bf8882099e870cd77dbcf00e94907855103c52
Philippe Marion <contact@nivocart.com>
2019-01-11 10:19:43 +0000

    New translations featured.php (Ukrainian)

---
+++ upload/catalog/language/Ukrainian/upload/catalog/language/english/module/featured.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title'] = 'Featured';
+
+// Text
+$_['text_from']     = 'from';
+$_['text_model']    = 'Code:';
+$_['text_reward']   = 'Reward Points:';
+$_['text_points']   = 'Price in Points:';
+$_['text_reviews']  = '(%s)';
