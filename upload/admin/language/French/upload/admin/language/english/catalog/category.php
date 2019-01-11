1b4813fee9fd4b75eccc44334ce44e52c5f32704
Philippe Marion <contact@nivocart.com>
2019-01-11 11:54:27 +0000

    New translations category.php (French)

--- upload/admin/language/French/upload/admin/language/english/catalog/category.php
+++ upload/admin/language/French/upload/admin/language/english/catalog/category.php
@@ -32,21 +32,21 @@ $_['entry_filter']           = 'Filtres :';
 $_['entry_store']            = 'Boutiques :';
 $_['entry_keyword']          = 'Mot-clé SEO :';
 $_['entry_image']            = 'Image :';
-$_['entry_top']              = 'Top:';
-$_['entry_column']           = 'Columns:';
-$_['entry_sort_order']       = 'Sort Order:';
-$_['entry_status']           = 'Status:';
-$_['entry_layout']           = 'Layout Override:';
+$_['entry_top']              = 'Haut :';
+$_['entry_column']           = 'Colonnes :';
+$_['entry_sort_order']       = 'Classement : ';
+$_['entry_status']           = 'État :';
+$_['entry_layout']           = 'Override de mise en page :';
 
 // Help
-$_['help_meta_description']  = 'The optimal number of characters is around 155, including spaces.<br />SEO importance: High.';
-$_['help_meta_keyword']      = 'Usually ignored by search engines.<br />SEO importance: Low.';
-$_['help_keyword']           = 'Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.';
-$_['help_top']               = 'Display in the top menu bar. Only works for the top parent categories.';
-$_['help_column']            = 'Number of columns to use for the bottom 3 categories. Only works for the top parent categories.';
+$_['help_meta_description']  = 'Le nombre optimal de caractères est environ 155, y compris les espaces.<br />SEO importance : haute.';
+$_['help_meta_keyword']      = 'Généralement ignorés par les moteurs de recherche.<br />SEO importance : faible.';
+$_['help_keyword']           = 'N\'utilisez pas d\'espaces, mais remplacez-les par des "-" et assurez-vous que le mot-clef soit absolument unique.';
+$_['help_top']               = 'Afficher dans la barre de menu du haut. Ne fonctionne que pour les catégories parentes.';
+$_['help_column']            = 'Nombre de colonnes à utiliser pour les catégories bas 3. Ne fonctionne que pour les catégories parentes.';
 
 // Error
-$_['error_permission']       = 'Warning: You do not have permission to modify <b>Categories</b> !';
-$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
-$_['error_name']             = 'Category Name must be between 2 and 32 characters!';
-$_['error_image']            = 'Image must be a valid image format!';
+$_['error_permission']       = 'Attention : vous n’êtes pas autorisé à modifier les <b>catégories</b> !';
+$_['error_warning']          = 'Attention: veuillez vérifier si le formulaire ne contient pas d\'erreurs !';
+$_['error_name']             = 'Le nom de la catégorie doit contenir entre 2 et 32 caractères !';
+$_['error_image']            = 'L\'image doit être dans un format valide!';
