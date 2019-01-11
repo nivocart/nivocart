d23bc63a1735de06ed0e8f1aa03f5491101cb568
Philippe Marion <contact@nivocart.com>
2019-01-11 10:52:49 +0000

    New translations special.php (Portuguese, Brazilian)

---
+++ upload/admin/language/Portuguese, Brazilian/upload/admin/language/english/module/special.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']         = 'Specials';
+
+// Text
+$_['text_module']           = 'Modules';
+$_['text_success']          = 'Success: You have modified module <b>Specials</b> !';
+
+// Entry
+$_['entry_theme']           = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']           = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+$_['entry_viewproduct']     = 'Show View Product Button:';
+$_['entry_addproduct']      = 'Show Add to Cart Button:<span class="help">Not recommended if some products have age restrictions.</span>';
+
+$_['entry_limit']           = 'Limit';
+$_['entry_image']           = 'Image (W x H)';
+$_['entry_layout']          = 'Layout';
+$_['entry_position']        = 'Position';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify module <b>Specials</b> !';
+$_['error_image']           = 'Image width &amp; height dimensions required!';
