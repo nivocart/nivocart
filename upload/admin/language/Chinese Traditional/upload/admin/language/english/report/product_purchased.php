ba0b972a6a0fadb45d77d5b0f7aff4bed4aa58aa
Philippe Marion <contact@nivocart.com>
2019-01-11 10:39:19 +0000

    New translations product_purchased.php (Chinese Traditional)

---
+++ upload/admin/language/Chinese Traditional/upload/admin/language/english/report/product_purchased.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']     = 'Products Purchased Report';
+
+// Text
+$_['text_all_status']   = 'All Statuses';
+
+// Column
+$_['column_name']       = 'Product Name';
+$_['column_model']      = 'Model';
+$_['column_price']      = 'Price';
+$_['column_cost']       = 'Cost';
+$_['column_quantity']   = 'Quantity';
+$_['column_subtotal']   = 'Sub-Total';
+$_['column_total']      = 'Total';
+
+// Entry
+$_['entry_date_start']  = 'Date Start:';
+$_['entry_date_end']    = 'Date End:';
+$_['entry_status']      = 'Order Status:';
