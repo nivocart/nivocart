2445743f91dd87521fb5874923ab9ca847c4773e
Philippe Marion <contact@nivocart.com>
2019-01-11 11:58:38 +0000

    New translations forgotten.php (French)

---
+++ upload/catalog/language/French/upload/catalog/language/english/affiliate/forgotten.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']   = 'Forgot Your Password?';
+
+// Text
+$_['text_account']    = 'Account';
+$_['text_forgotten']  = 'Forgotten Password';
+$_['text_your_email'] = 'Your Email Address';
+$_['text_email']      = 'Enter the Email address associated with your account. Click submit to have your password sent to you';
+$_['text_success']    = 'Success: An email with a confirmation link was sent to your Email address.';
+
+// Entry
+$_['entry_email']     = 'Email Address:';
+
+// Error
+$_['error_email']     = 'Email Address does not appear to be valid!';
+$_['error_unknown']   = 'Warning: The Email Address was not found in our records!';
+$_['error_approved']  = 'Warning: Your account requires approval before you can login.';
