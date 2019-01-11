9b5a0941c5d364182ac8e27be0fafaaaad0190f0
Philippe Marion <contact@nivocart.com>
2019-01-11 10:30:38 +0000

    New translations field.php (Chinese Simplified)

---
+++ upload/admin/language/Chinese Simplified/upload/admin/language/english/catalog/field.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']          = 'Fields';
+
+// Text
+$_['text_success']           = 'Success: You have modified <b>Fields</b> !';
+$_['text_default']           = 'Default';
+$_['text_info']              = '<b>Setup extras description fields for the product pages.</b><br /><br />Once created, the extra fields can be selected and configured under the "Fields" tab in the Product forms.';
+
+// Column
+$_['column_title']           = 'Field Title';
+$_['column_sort_order']      = 'Sort Order';
+$_['column_status']          = 'Status';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_title']            = 'Field Title:';
+$_['entry_description']      = 'Description:';
+$_['entry_status']           = 'Status:';
+$_['entry_sort_order']       = 'Sort Order:';
+
+// Button
+$_['button_info']            = 'Information';
+
+// Error
+$_['error_permission']       = 'Warning: You do not have permission to modify <b>Fields</b> !';
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
+$_['error_description']      = 'Description must be more than 3 characters!';
