7f5aaccfd9af85c03bdd5e47a509f7d4533c127c
Philippe Marion <contact@nivocart.com>
2019-01-11 11:58:01 +0000

    New translations english.php (French)

---
+++ upload/admin/language/French/French.php
@@ -0,0 +1,147 @@
+<?php
+// Locale
+$_['code']                          = 'fr';
+$_['direction']                     = 'ltr';
+$_['date_format_short']             = 'd/m/Y';
+$_['date_format_time']              = 'd/m/Y - H:i:s';
+$_['date_format_long']              = 'l d F Y';
+$_['time_format']                   = 'h:i:s';
+$_['decimal_point']                 = '.';
+$_['thousand_point']                = ',';
+
+// Text
+$_['text_yes']                      = 'Oui';
+$_['text_no']                       = 'Non';
+$_['text_enabled']                  = 'Activé';
+$_['text_disabled']                 = 'Désactivé';
+$_['text_deleted']                  = 'Supprimé';
+$_['text_none']                     = ' --- Aucun --- ';
+$_['text_select']                   = ' --- Sélectionner --- ';
+$_['text_select_all']               = 'Tout sélectionner';
+$_['text_unselect_all']             = 'Tout désélectionner';
+$_['text_all_countries']            = 'Tous les Pays';
+$_['text_all_zones']                = 'Toutes les Zones';
+$_['text_default']                  = ' <b>(Par défaut)</b>';
+$_['text_close']                    = 'Fermer';
+$_['text_pagination']               = 'Affichage de {start}-{end} des {total} résultats ({pages} Pages)';
+$_['text_no_results']               = 'Aucun résultat !';
+$_['text_separator']                = ' &gt; ';
+$_['text_edit']                     = 'Éditer';
+$_['text_view']                     = 'Afficher';
+$_['text_home']                     = 'Accueil';
+$_['text_characters']               = 'Caractères';
+
+// Text Content (Positions)
+$_['text_content_higher']           = '&#9710; &nbsp; Supérieur';
+$_['text_content_high']             = '&#9651; &nbsp; Haut';
+$_['text_content_left']             = '&#9665; &nbsp; Gauche';
+$_['text_content_right']            = '&#9655; &nbsp; Droit';
+$_['text_content_low']              = '&#9661; &nbsp; Bas';
+$_['text_content_lower']            = '&#10729 ; &nbsp; Inférieur';
+
+// Confirm Delete/Uninstall
+$_['text_confirm']                  = 'Supprimer/Désinstaller ne peut pas être annulé ! Êtes-vous sûr de que vouloir faire cela ?';
+$_['text_confirm_delete']           = 'Confirmer la Suppression';
+$_['text_confirm_uninstall']        = 'Confirmez la désinstallation';
+
+// Button
+$_['button_enable']                 = 'Activé';
+$_['button_disable']                = 'Désactivé';
+$_['button_insert']                 = 'Insérer';
+$_['button_delete']                 = 'Supprimer';
+$_['button_save']                   = 'Sauvegarder';
+$_['button_apply']                  = 'Appliquer';
+$_['button_cancel']                 = 'Annuler';
+$_['button_clear']                  = 'Effacer le journal';
+$_['button_close']                  = 'Fermer';
+$_['button_filter']                 = 'Flitrer';
+$_['button_send']                   = 'Envoyer';
+$_['button_edit']                   = 'Éditer';
+$_['button_copy']                   = 'Copier';
+$_['button_back']                   = 'Back';
+$_['button_remove']                 = 'Remove';
+$_['button_backup']                 = 'Backup';
+$_['button_restore']                = 'Restore';
+$_['button_repair']                 = 'Repair';
+$_['button_upload']                 = 'Upload';
+$_['button_download']               = 'Download';
+$_['button_submit']                 = 'Submit';
+$_['button_invoice']                = 'Invoice';
+$_['button_add_address']            = 'Add Address';
+$_['button_add_attribute']          = 'Add Attribute';
+$_['button_add_banner']             = 'Add Banner';
+$_['button_add_color']              = 'Add Color';
+$_['button_add_custom_field_value'] = 'Add Custom Field';
+$_['button_add_product']            = 'Add Product';
+$_['button_add_voucher']            = 'Add Voucher';
+$_['button_add_field']              = 'Add Field';
+$_['button_add_filter']             = 'Add Filter';
+$_['button_add_option']             = 'Add Option';
+$_['button_add_option_value']       = 'Add Option Value';
+$_['button_add_discount']           = 'Add Discount';
+$_['button_add_special']            = 'Add Special';
+$_['button_add_image']              = 'Add Image';
+$_['button_add_age_zone']           = 'Add Age Zone';
+$_['button_add_geo_zone']           = 'Add Geo Zone';
+$_['button_add_history']            = 'Add History';
+$_['button_add_transaction']        = 'Add Transaction';
+$_['button_add_total']              = 'Add Total';
+$_['button_add_profile']            = 'Add Profile';
+$_['button_add_reward']             = 'Add Reward Points';
+$_['button_add_route']              = 'Add Route';
+$_['button_add_rule' ]              = 'Add Rule';
+$_['button_add_module']             = 'Add Module';
+$_['button_add_link']               = 'Add Link';
+$_['button_update_total']           = 'Update Totals';
+$_['button_approve']                = 'Approve';
+$_['button_reset']                  = 'Reset';
+
+// Tab
+$_['tab_address']                   = 'Address';
+$_['tab_admin']                     = 'Admin';
+$_['tab_attribute']                 = 'Attribute';
+$_['tab_checkout']                  = 'Checkout';
+$_['tab_customer']                  = 'Customer Details';
+$_['tab_data']                      = 'Data';
+$_['tab_design']                    = 'Design';
+$_['tab_discount']                  = 'Discount';
+$_['tab_general']                   = 'General';
+$_['tab_history']                   = 'History';
+$_['tab_fields']                    = 'Fields';
+$_['tab_ftp']                       = 'Transfer';
+$_['tab_ip']                        = 'IP Addresses';
+$_['tab_links']                     = 'Links';
+$_['tab_colors']                    = 'Couleurs';
+$_['tab_log']                       = 'Journal';
+$_['tab_image']                     = 'Image';
+$_['tab_option']                    = 'Option';
+$_['tab_media']                     = 'Média';
+$_['tab_preference']                = 'Préférence';
+$_['tab_price']                     = 'Prix';
+$_['tab_server']                    = 'Serveur';
+$_['tab_store']                     = 'Boutique';
+$_['tab_special']                   = 'Spécial';
+$_['tab_local']                     = 'Local';
+$_['tab_mail']                      = 'Mail';
+$_['tab_module']                    = 'Module';
+$_['tab_order']                     = 'Détails de la commande';
+$_['tab_payment']                   = 'Détails de paiement';
+$_['tab_product']                   = 'Produits';
+$_['tab_return']                    = 'Détails de retour';
+$_['tab_reward']                    = 'Points de fidélité';
+$_['tab_profile']                   = 'Profil';
+$_['tab_shipping']                  = 'Détails d\'Expédition';
+$_['tab_total']                     = 'Totaux';
+$_['tab_transaction']               = 'Transactions';
+$_['tab_voucher']                   = 'Bons d\'achats';
+$_['tab_voucher_history']           = 'Histoire de bon d’achat';
+
+// Error
+$_['error_upload_1']                = 'ATTENTION: Le fichier téléchargé excède la directive upload_max_filesize dans php.ini !';
+$_['error_upload_2']                = 'ATTENTION: Le fichier téléchargé excède la directive MAX_FILE_SIZE qui a été spécifiée dans le formulaire HTML !';
+$_['error_upload_3']                = 'Attention: Le fichier n\'a été que partiellement envoyé !';
+$_['error_upload_4']                = 'Attention: Aucun fichier n\'a été envoyé !';
+$_['error_upload_6']                = 'Attention : le dossier temporaire est manquant !';
+$_['error_upload_7']                = 'Attention : impossible d’écrire le fichier sur le disque !';
+$_['error_upload_8']                = 'Attention : envoi de fichier bloqué par extension !';
+$_['error_upload_999']              = 'Attention : aucun code d’erreur disponible !';
