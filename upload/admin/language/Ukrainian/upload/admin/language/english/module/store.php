8e609435c096d6fe5422d79d288bfb162fe729d5
Philippe Marion <contact@nivocart.com>
2019-01-11 10:22:49 +0000

    New translations store.php (Ukrainian)

---
+++ upload/admin/language/Ukrainian/upload/admin/language/english/module/store.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']         = 'Store';
+
+// Text
+$_['text_module']           = 'Modules';
+$_['text_success']          = 'Success: You have modified module <b>Store</b> !';
+$_['text_permission']       = 'Admin Users Only:';
+$_['text_access']           = 'Admin Button:';
+
+// Entry
+$_['entry_theme']           = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']           = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+
+$_['entry_layout']          = 'Layout';
+$_['entry_permission']      = 'Permission';
+$_['entry_access']          = 'Access';
+$_['entry_position']        = 'Position';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify module <b>Store</b> !';
