0a1206259357e253f7d116c742b467058235f135
Philippe Marion <contact@nivocart.com>
2019-01-11 12:05:08 +0000

    New translations cheque.php (Spanish)

---
+++ upload/admin/language/Spanish/upload/admin/language/english/payment/cheque.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']      = 'Cheque / Money Order';
+
+// Link
+$_['text_cheque']        = '';
+
+// Text
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified payment <b>Cheque / Money Order</b> !';
+
+// Entry
+$_['entry_payable']      = 'Payable To';
+$_['entry_total']        = 'Total';
+$_['entry_total_max']    = 'Total Maximum';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes <b>active</b>';
+$_['help_total_max']     = 'The maximum checkout total the order must reach before this payment method becomes <b>inactive</b>.<br />Leave empty for no maximum.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment <b>Cheque / Money Order</b> !';
+$_['error_payable']      = 'Payable To required!';
