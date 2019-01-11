8f1d8673ccad4e5c9b726d46a9f9f3bdc263dcb2
Philippe Marion <contact@nivocart.com>
2019-01-11 12:01:25 +0000

    New translations slideshow.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/module/slideshow.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']       = 'Slideshow';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module <b>Slideshow</b>!';
+
+// Entry
+$_['entry_theme']         = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']         = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+$_['entry_transition']    = 'Select Transitions:';
+$_['entry_duration']      = 'Frame Duration:';
+$_['entry_speed']         = 'Animation Speed:';
+$_['entry_random']        = 'Shuffle Images:';
+$_['entry_dots']          = 'Show Dots:';
+$_['entry_arrows']        = 'Show Arrows:';
+
+$_['entry_banner']        = 'Banner';
+$_['entry_dimension']     = 'Dimension (W x H)';
+$_['entry_layout']        = 'Layout';
+$_['entry_position']      = 'Position';
+$_['entry_status']        = 'Status';
+$_['entry_sort_order']    = 'Sort Order';
+
+// Button
+$_['button_manager']      = 'Banner Manager';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module <b>Slideshow</b>!';
+$_['error_dimension']     = 'Width &amp; Height dimensions required!';
