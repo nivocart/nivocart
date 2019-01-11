086d7fd4a623003535b164a408994f6891a518b1
Philippe Marion <contact@nivocart.com>
2019-01-11 12:02:32 +0000

    New translations google_sitemap.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/feed/google_sitemap.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Google Sitemap';
+
+// Text
+$_['text_feed']        = 'Flux de produits';
+$_['text_success']     = 'Succès : vous avez modifié le <b>flux Google Sitemap</b> !';
+
+// Entry
+$_['entry_status']     = 'État :';
+$_['entry_data_feed']  = 'URL du flux :';
+
+// Error
+$_['error_permission'] = 'Attention ! Vous n\'êtes pas autorisé à modifier le <b>flux Google Sitemap</b> !';
