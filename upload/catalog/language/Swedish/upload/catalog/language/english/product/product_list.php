254dad9c8bef64fd1b2393618c452237c4d78820
Philippe Marion <contact@nivocart.com>
2019-01-11 10:05:43 +0000

    New translations product_list.php (Swedish)

---
+++ upload/catalog/language/Swedish/upload/catalog/language/english/product/product_list.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']         = 'Product List';
+
+// Text
+$_['text_error']            = 'Products not found!';
+$_['text_empty']            = 'There are no products to display.';
+$_['text_total_categories'] = 'Categories';
+$_['text_total_products']   = 'Products';
+$_['text_title_product']    = 'Products';
+$_['text_to']               = 'to';
+$_['text_in']               = 'in';
