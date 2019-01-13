5652159c5bba4c19cc4bfaece88f9f45e7d6862f
Philippe Marion <contact@nivocart.com>
2019-01-13 15:32:57 +0000

    New translations storeya.php (Filipino)

---
+++ upload/admin/language/Filipino/upload/admin/language/english/feed/storeya.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'StoreYa';
+
+// Text
+$_['text_feed']        = 'Product Feeds';
+$_['text_success']     = 'Success: You have modified <b>StoreYa</b> feed!';
+$_['text_link']        = '<span class="help">Click to go to the StoreYa page, choose a plan and then paste the link in the "Data Feed Url" field above!</span>';
+
+// Entry
+$_['entry_status']     = 'Status:';
+$_['entry_limits']     = 'Product Limit:';
+$_['entry_currency']   = 'Currency';
+$_['entry_language']   = 'Language';
+$_['entry_data_feed']  = 'Data Feed Url:';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify <b>StoreYa</b> feed!';
