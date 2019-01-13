bbefb3cfa413098eb6b50c798964f6d237429a7f
Philippe Marion <contact@nivocart.com>
2019-01-13 15:24:24 +0000

    Update mediaplayer.php
    Update version

--- upload/admin/controller/module/mediaplayer.php
+++ upload/admin/controller/module/mediaplayer.php
@@ -3,7 +3,7 @@ class ControllerModuleMediaPlayer extends Controller {
 	private $error = array();
 	private $_name = 'mediaplayer';
 	private $_plugin = 'Plyr';
-	private $_version = 'v2.0.18';
+	private $_version = 'v3.4.7';
 
 	public function index() {
 		$this->language->load('module/' . $this->_name);
