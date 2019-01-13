14a489135552829498c482e8a66a62d56e49b470
Philippe Marion <contact@nivocart.com>
2019-01-13 15:13:30 +0000

    New translations payment.php (Hindi)

---
+++ upload/admin/language/Hindi/upload/admin/language/english/extension/payment.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']     = 'Payments';
+
+// Text
+$_['text_install']      = 'Install';
+$_['text_uninstall']    = 'Uninstall';
+$_['text_is_recurring'] = 'Accepts recurring transactions';
+
+// Column
+$_['column_name']       = 'Payment Method';
+$_['column_status']     = 'Status';
+$_['column_sort_order'] = 'Sort Order';
+$_['column_action']     = 'Action';
+
+// Button
+$_['button_images']     = 'Payment Images';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify <b>Payments</b> !';
