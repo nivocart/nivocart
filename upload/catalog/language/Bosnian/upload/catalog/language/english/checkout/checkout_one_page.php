47e29ae6a28684bbe53ba10bc88cd693ac345073
Philippe Marion <contact@nivocart.com>
2019-01-13 15:46:52 +0000

    New translations checkout_one_page.php (Bosnian)

---
+++ upload/catalog/language/Bosnian/upload/catalog/language/english/checkout/checkout_one_page.php
@@ -0,0 +1,101 @@
+<?php
+// Heading
+$_['heading_title']                           = 'Checkout';
+
+// Text
+$_['text_cart']                               = 'Shopping Cart';
+$_['text_checkout_payment_address']           = 'Billing Details';
+$_['text_checkout_shipping_address']          = 'Delivery Details';
+$_['text_female']                             = 'Female';
+$_['text_male']                               = 'Male';
+$_['text_agree']                              = 'I have read and agree to the <a class="colorbox" href="%s" title="%s"><b>%s</b></a>';
+$_['text_shipping_method']                    = 'Shipping method';
+$_['text_payment_method']                     = 'Payment method';
+$_['text_comments']                           = 'Delivery Instructions';
+$_['text_one_page_coupon']                    = 'Coupon Code';
+$_['text_one_page_voucher']                   = 'Gift Voucher';
+$_['text_one_page_reward']                    = 'Reward Points (%s)';
+$_['text_recurring_item']                     = 'Recurring item';
+$_['text_payment_profile']                    = 'Payment Profile';
+$_['text_trial_description']                  = '%s every %d %s(s) for %d payment(s) then';
+$_['text_payment_description']                = '%s every %d %s(s) for %d payment(s)';
+$_['text_payment_until_canceled_description'] = '%s every %d %s(s) until canceled';
+$_['text_day']                                = 'day';
+$_['text_week']                               = 'week';
+$_['text_semi_month']                         = 'half-month';
+$_['text_month']                              = 'month';
+$_['text_year']                               = 'year';
+
+// Column
+$_['column_name']                             = 'Product';
+$_['column_model']                            = 'Model';
+$_['column_quantity']                         = 'Qty';
+$_['column_price']                            = 'Price';
+$_['column_tax_value']                        = 'Tax';
+$_['column_tax_percent']                      = 'Tax %';
+$_['column_total']                            = 'Total';
+
+// Entry
+$_['entry_firstname']                         = 'First Name';
+$_['entry_lastname']                          = 'Last Name';
+$_['entry_email']                             = 'Email';
+$_['entry_telephone']                         = 'Telephone';
+$_['entry_fax']                               = 'Fax';
+$_['entry_gender']                            = 'Gender';
+$_['entry_date_of_birth']                     = 'Date of Birth';
+$_['entry_company']                           = 'Company';
+$_['entry_customer_group']                    = 'Business Type';
+$_['entry_company_id']                        = 'Company ID';
+$_['entry_tax_id']                            = 'Tax ID';
+$_['entry_address_1']                         = 'Address 1';
+$_['entry_address_2']                         = 'Address 2';
+$_['entry_postcode']                          = 'Post code';
+$_['entry_city']                              = 'City';
+$_['entry_country']                           = 'Country';
+$_['entry_zone']                              = 'Region / State';
+$_['entry_shipping']                          = 'Deliver to this address';
+$_['entry_coupon']                            = 'Enter your coupon here:';
+$_['entry_voucher']                           = 'Enter your gift voucher code here:';
+$_['entry_reward']                            = 'Enter the number of points to use here (Maximum %s):';
+
+// Button
+$_['button_wrapping_add']                     = 'Add';
+$_['button_wrapping_remove']                  = 'Remove';
+
+// Success
+$_['text_coupon']                             = 'Success: Your coupon discount has been applied!';
+$_['text_voucher']                            = 'Success: Your gift voucher discount has been applied!';
+$_['text_reward']                             = 'Success: Your reward points discount has been applied!';
+
+// Error
+$_['error_warning']                           = 'There was a problem while trying to process your order! If the problem persists please try selecting a different payment method or you can contact the store owner by <a href="%s">clicking here</a>.';
+$_['error_firstname']                         = 'First Name must be between 1 and 32 characters!';
+$_['error_lastname']                          = 'Last Name must be between 1 and 32 characters!';
+$_['error_email']                             = 'Email Address does not appear to be valid!';
+$_['error_exists']                            = 'Email Address is already registered!';
+$_['error_telephone']                         = 'Telephone must be between 3 and 32 characters!';
+$_['error_date_of_birth']                     = 'Date of Birth is required and must be valid!';
+$_['error_company_id']                        = 'Company ID required!';
+$_['error_tax_id']                            = 'Tax ID required!';
+$_['error_vat']                               = 'VAT number is invalid!';
+$_['error_address_1']                         = 'Address 1 must be between 3 and 128 characters!';
+$_['error_city']                              = 'City must be between 2 and 128 characters!';
+$_['error_postcode']                          = 'Postcode must be between 2 and 10 characters!';
+$_['error_country']                           = 'Please select a country!';
+$_['error_zone']                              = 'Please select a region / state!';
+$_['error_agree']                             = 'You must read and agree to the %s!';
+$_['error_shipping']                          = 'Shipping method is required!';
+$_['error_no_shipping']                       = 'No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_payment']                           = 'Payment method is required!';
+$_['error_no_payment']                        = 'No Payment options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_coupon']                            = 'Warning: Coupon is either invalid, expired or reached it\'s usage limit!';
+$_['error_voucher']                           = 'Warning: Gift Voucher is either invalid or the balance has been used up!';
+$_['error_reward']                            = 'Warning: Please enter the amount of reward points to use!';
+$_['error_points']                            = 'Warning: You don\'t have %s reward points!';
+$_['error_maximum']                           = 'Warning: The maximum number of points that can be applied is %s!';
+$_['error_minimum']                           = 'Minimum order amount for %s is %s!';
+$_['error_required']                          = '%s required!';
+
+$_['text_trial']                              = '%s every %s %s for %s payments then ';
+$_['text_recurring']                          = '%s every %s %s';
+$_['text_length']                             = ' for %s payments';
