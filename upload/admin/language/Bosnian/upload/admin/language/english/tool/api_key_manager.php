c1e031cf6178d5ea5ddad8e7cd7096ee8b431f0d
Philippe Marion <contact@nivocart.com>
2019-01-13 15:44:16 +0000

    New translations api_key_manager.php (Bosnian)

---
+++ upload/admin/language/Bosnian/upload/admin/language/english/tool/api_key_manager.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']    = 'API Key Manager';
+
+// Text
+$_['text_success']     = 'Success: You have modified <b>API Key Manager</b> !';
+$_['text_default']     = 'Default';
+$_['text_api_info']    = 'The API Key Manager main purpose is to help you store all your important API Keys in one place.';
+
+// Column
+$_['column_name']      = 'Key Name';
+$_['column_code']      = 'Key Code';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Action';
+
+// Entry
+$_['entry_name']       = 'Key Name:';
+$_['entry_code']       = 'Key Code:';
+$_['entry_status']     = 'Status:';
+
+// Links
+$_['link_google']      = 'Google APIs';
+$_['link_facebook']    = 'Facebook APIs';
+$_['link_twitter']     = 'Twitter APIs';
+
+// Button
+$_['button_info']      = 'Information';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify <b>API Key Manager</b> !';
+$_['error_name']       = 'Key Name must be between 3 and 64 characters!';
+$_['error_code']       = 'Key Code must be between 3 and 128 characters!';
