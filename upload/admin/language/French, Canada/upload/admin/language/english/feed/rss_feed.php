bb40eb021e118d8caa3cd07d63977ff5d86a59ce
Philippe Marion <contact@nivocart.com>
2019-01-13 15:21:37 +0000

    New translations rss_feed.php (French, Canada)

---
+++ upload/admin/language/French, Canada/upload/admin/language/english/feed/rss_feed.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']     = 'Flux RSS de produits';
+
+// Text
+$_['text_feed']         = 'Flux RSS des produits';
+$_['text_success']      = 'Success: You have successfully modified <b>RSS Product Feed</b> !';
+
+// Entry
+$_['entry_status']      = 'Status:';
+$_['entry_limit']       = 'Product Limit:<span class="help">The amount of products listed on the feed. Sites that regularly add new products should set a high number here.</span>';
+$_['entry_show_image']  = 'Afficher l\'image :';
+$_['entry_show_price']  = 'Afficher les prix :';
+$_['entry_include_tax'] = 'Taxe incluse dans le prix :';
+$_['entry_image_size']  = 'Taille de l\'image :';
+$_['entry_data_feed']   = 'Data Feed Url:<span class="help">This Url points to your feed.<br />(Not editable)</span>';
+
+// Button
+$_['button_save']       = 'Sauvegarder';
+$_['button_cancel']     = 'Annuler';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify <b>RSS Product Feed</b> !';
+$_['error_integer']     = 'La limite doit être un nombre entier!';
