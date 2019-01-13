31946f9826e0894bd226811eeb622c9101f30b2b
Philippe Marion <contact@nivocart.com>
2019-01-13 15:21:29 +0000

    New translations language.php (French, Canada)

---
+++ upload/admin/language/French, Canada/upload/admin/language/english/localisation/language.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']     = 'Language';
+
+// Text
+$_['text_success']      = 'Success: You have modified <b>Languages</b> !';
+
+// Column
+$_['column_name']       = 'Language Name';
+$_['column_code']       = 'Code';
+$_['column_image']      = 'Image';
+$_['column_sort_order'] = 'Sort Order';
+$_['column_status']     = 'Status';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_name']        = 'Language Name:';
+$_['entry_code']        = 'Code:<span class="help">Example: en. Do not change if this is your default language.</span>';
+$_['entry_locale']      = 'Locale:<span class="help">Example: en-US,en_US.UTF-8,en_US,en-gb,english</span>';
+$_['entry_image']       = 'Image:<span class="help">Example: gb.png</span>';
+$_['entry_directory']   = 'Directory:<span class="help">Name of the language directory (case-sensitive).</span>';
+$_['entry_filename']    = 'Filename:<span class="help">Main language filename without extension.</span>';
+$_['entry_status']      = 'Status:<span class="help">Hide/Show it in language dropdown.</span>';
+$_['entry_sort_order']  = 'Sort Order:';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify <b>Languages</b> !';
+$_['error_name']        = 'Language Name must be between 3 and 32 characters!';
+$_['error_code']        = 'Language Code must at least 2 characters!';
+$_['error_locale']      = 'Locale required!';
+$_['error_image']       = 'Image Filename must be between 3 and 64 characters!';
+$_['error_directory']   = 'Directory required!';
+$_['error_filename']    = 'Filename must be between 3 and 64 characters!';
+$_['error_default']     = 'Warning: This language cannot be deleted as it is currently assigned as the default store language!';
+$_['error_admin']       = 'Warning: This language cannot be deleted as it is currently assigned as the administration language!';
+$_['error_store']       = 'Warning: This language cannot be deleted as it is currently assigned to %s stores!';
+$_['error_order']       = 'Warning: This language cannot be deleted as it is currently assigned to %s orders!';
