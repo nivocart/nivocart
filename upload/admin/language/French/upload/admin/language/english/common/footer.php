2e181ccc83f69030929c45c1ef8b5773449302cf
Philippe Marion <contact@nivocart.com>
2019-01-14 15:06:36 +0000

    New translations footer.php (French)

--- upload/admin/language/French/upload/admin/language/english/common/footer.php
+++ upload/admin/language/French/upload/admin/language/english/common/footer.php
@@ -1,3 +1,3 @@
 <?php
 // Text
-$_['text_footer'] = '<a href="https://nivocart.com">NivoCart</a> &copy; ' . date('Y') . ' All Rights Reserved.<br />Version %s';
+$_['text_footer'] = '<a href="https://nivocart.com">NivoCart</a> &copy; ' . date('Y') . ' Tous Droits Réservés.<br />Version %s';
