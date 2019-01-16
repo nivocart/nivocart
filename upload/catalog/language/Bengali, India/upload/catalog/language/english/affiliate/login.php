0fa0844eb6a5dce4ab3ee0d5c32382e81ac91f8b
Philippe Marion <contact@nivocart.com>
2019-01-16 13:51:54 +0000

    New translations login.php (Bengali, India)

---
+++ upload/catalog/language/Bengali, India/upload/catalog/language/english/affiliate/login.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Affiliate Program';
+
+// Text
+$_['text_account']                  = 'Account';
+$_['text_login']                    = 'Login';
+$_['text_description']              = '<p>The %s affiliate program is a free service and enables members to earn revenue by placing a link or links on their website which advertises %s or specific products on it. Any sales made to customers who have clicked on those links will earn the affiliate commission. The standard commission rate is currently %s.</p><p>For more information, see our Affiliate terms &amp; conditions.</p>';
+$_['text_new_affiliate']            = 'New Affiliate';
+$_['text_register_account']         = '<p><b>I am not currently an affiliate.</b></p><p>Click <b>Continue</b> below to create a new affiliate account. New accounts may require approval before you can login. Check your email inbox for an approval confirmation. Please note that this is not connected in any way to your customer account.</p>';
+$_['text_returning_affiliate']      = 'Affiliate Login';
+$_['text_i_am_returning_affiliate'] = '<b>I am a returning affiliate.</b>';
+$_['text_forgotten']                = 'Forgotten Password';
+
+// Entry
+$_['entry_email']                   = 'Affiliate Email:';
+$_['entry_password']                = 'Password:';
+
+// Error
+$_['error_login']                   = 'Warning: No match for Email Address and/or Password.';
+$_['error_attempts']                = 'Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.';
+$_['error_approved']                = 'Warning: Your account requires approval before you can login.';
