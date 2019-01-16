b0d9f9682b8a072f61a363bcf53018ef5e7f8805
Philippe Marion <contact@nivocart.com>
2019-01-16 13:51:35 +0000

    New translations filter.php (Armenian)

---
+++ upload/admin/language/Armenian/upload/admin/language/english/module/filter.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']       = 'Filter';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module <b>Filter</b> !';
+
+// Entry
+$_['entry_theme']         = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']         = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+
+$_['entry_layout']        = 'Layout';
+$_['entry_position']      = 'Position';
+$_['entry_status']        = 'Status';
+$_['entry_sort_order']    = 'Sort Order';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module <b>Filter</b> !';
