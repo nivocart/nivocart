42c81d74163e17895bde11709f76cc402f76bf47
Philippe Marion <contact@nivocart.com>
2019-01-16 13:49:44 +0000

    New translations cod.php (Armenian)

---
+++ upload/admin/language/Armenian/upload/admin/language/english/payment/cod.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']      = 'Cash On Delivery';
+
+// Link
+$_['text_cod']           = '';
+
+// Text
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified payment <b>Cash On Delivery</b> !';
+
+// Entry
+$_['entry_total']        = 'Total';
+$_['entry_total_max']    = 'Total Maximum';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes <b>active</b>';
+$_['help_total_max']     = 'The maximum checkout total the order must reach before this payment method becomes <b>inactive</b>.<br />Leave empty for no maximum.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment <b>Cash On Delivery</b> !';
