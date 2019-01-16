1b2567b74038518384455dd203f4f466ab534765
Philippe Marion <contact@nivocart.com>
2019-01-16 13:37:24 +0000

    New translations menu.php (Arabic)

---
+++ upload/admin/language/Arabic/upload/admin/language/english/design/menu.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title']       = 'Menus';
+
+// Text
+$_['text_insert_success'] = 'Success: Your <b>Menu</b> has been Created !';
+$_['text_update_success'] = 'Success: You have Modified a <b>Menu</b> !';
+$_['text_delete_success'] = 'Success: You have Deleted a <b>Menu</b> !';
+$_['text_enabled']        = 'Enabled';
+$_['text_disabled']       = 'Disabled';
+$_['text_default']        = 'Default';
+$_['text_menu_item_add']  = 'Add Item';
+$_['text_menu_item_view'] = 'View Items';
+$_['text_menu_items']     = 'Menu Items';
+$_['text_menu_edit']      = 'Edit';
+$_['text_menu_info']      = 'First, click the "Insert" button above to create a new menu.<br />
+Once created, it will be displayed in the list below and you will be able to add new menu items inside it. The number of menus is unlimited, as is the number of menu items.<br />
+You can also Edit or Add menu items directly for an already existing menu by clicking the "View Items" or "Add Item" buttons respectively.<br />
+Menus are just passive lists of names and links. They require a module such as "Menu Horizontal", "Menu Vertical" or other menu modules, to display the menu items with their own specific styles.';
+
+// Column
+$_['column_title']        = 'Menu Title';
+$_['column_menu_items']   = 'Menu Items';
+$_['column_status']       = 'Status';
+$_['column_action']       = 'Action';
+
+// Entry
+$_['entry_title']         = 'Menu Title:';
+$_['entry_store']         = 'Stores:';
+$_['entry_status']        = 'Status:';
+
+// Button
+$_['button_info']         = 'Information';
+$_['button_vertical']     = 'Menu Vertical';
+$_['button_horizontal']   = 'Menu Horizontal';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify <b>Menus</b> !';
+$_['error_warning']       = 'Warning: Please check the form carefully for errors !';
+$_['error_title']         = 'Menu Title must be between 2 and 32 characters !';
