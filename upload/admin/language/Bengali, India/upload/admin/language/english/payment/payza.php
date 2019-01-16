42f4ce8395adf7dc67fa4b4d77bc12985dc16460
Philippe Marion <contact@nivocart.com>
2019-01-16 13:58:17 +0000

    New translations payza.php (Bengali, India)

---
+++ upload/admin/language/Bengali, India/upload/admin/language/english/payment/payza.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']      = 'Payza';
+
+// Link
+$_['text_payza']         = '<a onclick="window.open(\'https://www.payza.com/\');"><img src="view/image/payment/payza.png" alt="Payza" title="Payza" style="border:1px solid #EEEEEE;" /></a>';
+
+// Text
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified <b>Payza</b> account details !';
+
+// Entry
+$_['entry_merchant']     = 'Merchant ID';
+$_['entry_security']     = 'Security Code';
+$_['entry_callback']     = 'Alert URL';
+$_['entry_total']        = 'Total';
+$_['entry_total_max']    = 'Total Maximum';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_callback']      = 'This has to be set in the Payza control panel. You will also need to check the "IPN Status" to enabled.';
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
+$_['help_total_max']     = 'The maximum checkout total the order must reach before this payment method becomes <b>inactive</b>.<br />Leave empty for no maximum.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment <b>Payza</b> !';
+$_['error_merchant']     = 'Merchant ID Required!';
+$_['error_security']     = 'Security Code Required!';
