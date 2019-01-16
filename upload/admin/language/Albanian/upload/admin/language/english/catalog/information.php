f693e1c6964eaa67d58aa284d51685658a0ce7ce
Philippe Marion <contact@nivocart.com>
2019-01-16 13:35:14 +0000

    New translations information.php (Albanian)

---
+++ upload/admin/language/Albanian/upload/admin/language/english/catalog/information.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']          = 'Information';
+
+// Text
+$_['text_success']           = 'Success: You have modified <b>Information</b> !';
+$_['text_default']           = 'Default';
+
+// Column
+$_['column_title']           = 'Information Title';
+$_['column_sort_order']      = 'Sort Order';
+$_['column_bottom']          = 'Footer';
+$_['column_status']          = 'Status';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_title']            = 'Information Title:';
+$_['entry_meta_description'] = 'Meta Tag Description:';
+$_['entry_meta_keyword']     = 'Meta Tag Keywords:';
+$_['entry_description']      = 'Description:';
+$_['entry_store']            = 'Stores:';
+$_['entry_keyword']          = 'SEO Keyword:';
+$_['entry_bottom']           = 'Bottom:';
+$_['entry_status']           = 'Status:';
+$_['entry_sort_order']       = 'Sort Order:';
+$_['entry_layout']           = 'Layout Override:';
+
+// Help
+$_['help_meta_description']  = 'The optimal number of characters is around 155, including spaces.<br />SEO importance: High.';
+$_['help_meta_keyword']      = 'Usually ignored by search engines.<br />SEO importance: Low.';
+$_['help_keyword']           = 'Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.';
+$_['help_bottom']            = 'Display in the bottom footer.';
+
+// Error
+$_['error_permission']       = 'Warning: You do not have permission to modify <b>Information</b> !';
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
+$_['error_description']      = 'Description must be more than 3 characters!';
+$_['error_account']          = 'Warning: This information page cannot be deleted as it is currently assigned as the store account terms!';
+$_['error_checkout']         = 'Warning: This information page cannot be deleted as it is currently assigned as the store checkout terms!';
+$_['error_affiliate']        = 'Warning: This information page cannot be deleted as it is currently assigned as the store affiliate terms!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
