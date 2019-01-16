f36a025234f9f7700e1b8b5ec94bd0a9cb77003c
Philippe Marion <contact@nivocart.com>
2019-01-16 13:24:32 +0000

    New translations palette.php (Afrikaans)

---
+++ upload/admin/language/Afrikaans/upload/admin/language/english/catalog/palette.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']    = 'Palettes';
+
+// Text
+$_['text_success']     = 'Success: You have modified <b>Palettes</b> !';
+$_['text_default']     = 'Default';
+$_['text_info']        = '<b>Setup color palettes for the product pages.</b><br /><br />Once created, the palettes can be selected and configured under the "Colors" tab in the Product forms.';
+
+// Column
+$_['column_name']      = 'Palette Name';
+$_['column_colors']    = 'Colors';
+$_['column_action']    = 'Action';
+
+// Entry
+$_['entry_name']       = 'Palette Name:';
+$_['entry_title']      = 'Color Title';
+$_['entry_color']      = 'Hexadecimal Color';
+$_['entry_skin']       = 'Search Group';
+
+// Button
+$_['button_info']      = 'Information';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify <b>Palettes</b> !';
+$_['error_name']       = 'Palette Name must be between 3 and 64 characters!';
+$_['error_title']      = 'Color Title must be between 3 and 64 characters!';
+$_['error_color']      = 'Color must be a valid hexadecimal value and contain no less than 6 characters!';
