1ee65efc3fc3c9dcff38f48ccc63cda15140ec7a
Philippe Marion <contact@nivocart.com>
2019-01-16 13:39:50 +0000

    New translations order.php (Arabic)

---
+++ upload/catalog/language/Arabic/upload/catalog/language/english/account/order.php
@@ -0,0 +1,53 @@
+<?php
+// Heading
+$_['heading_title']           = 'Order History';
+$_['heading_title_pick_list'] = 'Order Picking';
+
+// Column
+$_['column_name']             = 'Product';
+$_['column_model']            = 'Model';
+$_['column_quantity']         = 'Quantity';
+$_['column_price']            = 'Unit Price';
+$_['column_tax_value']        = 'Tax';
+$_['column_tax_percent']      = 'Tax %';
+$_['column_total']            = 'Total';
+$_['column_action']           = 'Action';
+$_['column_date_added']       = 'Date Added';
+$_['column_status']           = 'Status';
+$_['column_comment']          = 'Comment';
+$_['column_order_picked']     = 'Picked';
+$_['column_order_none']       = 'Pending';
+
+// Text
+$_['text_account']            = 'Account';
+$_['text_order']              = 'Order Information';
+$_['text_order_detail']       = 'Order Details';
+$_['text_order_invoice']      = 'Invoice Duplicate';
+$_['text_invoice_no']         = 'Invoice N&deg;:';
+$_['text_order_id']           = 'Order ID:';
+$_['text_status']             = 'Status:';
+$_['text_telephone']          = 'Telephone:';
+$_['text_fax']                = 'Fax:';
+$_['text_date_added']         = 'Date Added:';
+$_['text_customer']           = 'Customer:';
+$_['text_shipping_address']   = 'Shipping Address';
+$_['text_shipping_method']    = 'Shipping Method:';
+$_['text_payment_address']    = 'Payment Address';
+$_['text_payment_method']     = 'Payment Method:';
+$_['text_products']           = 'Products:';
+$_['text_total']              = 'Total:';
+$_['text_order_pick_list']    = 'Order Picking';
+$_['text_comment']            = 'Order Comments';
+$_['text_history']            = 'Order History';
+$_['text_damages']            = 'ANY DAMAGES, SHORTAGES OR INCORRECT GOODS MUST BE REPORTED WITHIN 14 DAYS';
+$_['text_bank_name']          = '<b>Registered Bank:</b>';
+$_['text_bank_account']       = '<b>Account N&deg;:</b>';
+$_['text_copyrights']         = '&copy; ' . date('Y') . ' All Rights Reserved';
+
+$_['text_success']            = 'You have successfully added the products from order ID #%s to your cart!';
+$_['text_empty']              = 'You have not made any previous orders!';
+$_['text_error']              = 'The order you requested could not be found!';
+
+// Button
+$_['button_invoice']          = 'Download Invoice';
+$_['button_pick']             = 'Pick List';
