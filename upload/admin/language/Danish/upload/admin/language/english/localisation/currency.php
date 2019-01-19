35e6e8ace4ea36a962d6760ed350a206f2c60880
Philippe Marion <contact@nivocart.com>
2019-01-19 07:35:07 +0000

    New translations currency.php (Danish)

---
+++ upload/admin/language/Danish/upload/admin/language/english/localisation/currency.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']        = 'Currency';
+
+// Text
+$_['text_success']         = 'Success: You have modified <b>Currencies</b> !';
+
+// Column
+$_['column_title']         = 'Currency Title';
+$_['column_code']          = 'Code';
+$_['column_status']        = 'Status';
+$_['column_value']         = 'Value';
+$_['column_date_modified'] = 'Last Update';
+$_['column_action']        = 'Action';
+
+// Entry
+$_['entry_title']          = 'Currency Title:';
+$_['entry_code']           = 'Code:<span class="help">Do not change if this is your default currency. Must be a valid <a onclick="window.open(\'http://www.xe.com/iso4217.php\');"><b>ISO code</b></a>.</span>';
+$_['entry_value']          = 'Value:<span class="help">Set to 1.00000 if this is your default currency.</span>';
+$_['entry_symbol_left']    = 'Symbol Left:';
+$_['entry_symbol_right']   = 'Symbol Right:';
+$_['entry_decimal_place']  = 'Decimal Places:';
+$_['entry_status']         = 'Status:';
+
+// Help
+$_['help_currencies']      = 'Latest currency rates will be available daily at 12:01 GMT.';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to modify <b>Currencies</b> !';
+$_['error_title']          = 'Currency Title must be between 3 and 32 characters!';
+$_['error_code']           = 'Currency Code must contain 3 characters!';
+$_['error_default']        = 'Warning: This currency cannot be deleted as it is currently assigned as the default store currency!';
+$_['error_store']          = 'Warning: This currency cannot be deleted as it is currently assigned to %s stores!';
+$_['error_order']          = 'Warning: This currency cannot be deleted as it is currently assigned to %s orders!';
