1b1cee8cfcee5ebe91f8604e4173962df0271194
Philippe Marion <contact@nivocart.com>
2019-01-20 15:24:40 +0000

    New translations author.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/blog/author.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']          = 'Auteur du blog';
+
+// Columns
+$_['column_author_name']     = 'Nom de l’auteur';
+$_['column_status']          = 'État ';
+$_['column_date_added']      = 'Date d’ajout';
+$_['column_action']          = 'Action';
+
+// Text
+$_['text_success']           = 'Succès : Vous avez modifié des auteurs de Blog !';
+$_['text_default']           = 'Par défaut';
+$_['text_image_manager']     = 'Gestionnaire de fichiers';
+$_['text_browse']            = 'Parcourir';
+$_['text_clear']             = 'Effacer';
+
+// Entry
+$_['entry_name']             = 'Nom de l’auteur : nom de l’auteur<span class="help">doit être unique.</span>';
+$_['entry_image']            = 'Avatar:';
+$_['entry_meta_description'] = 'Meta Tag Description :<span class="help">le nombre optimal de caractères est environ 155, y compris les espaces.<br />SEO importance : haute.</span>';
+$_['entry_meta_keyword']     = 'Mot clé Meta :<span class="help">généralement ignorés par les moteurs de recherche.<br />SEO importance : faible.</span>';
+$_['entry_description']      = 'Description :';
+$_['entry_status']           = 'État :';
+
+// Errors
+$_['error_warning']          = 'Attention: veuillez vérifier si le formulaire ne contient pas d\'erreurs !';
+$_['error_permission']       = 'Avertissement : Vous n’êtes pas autorisé à modifier les auteurs !';
+$_['error_name']             = 'Le nom de l’auteur doit contenir entre 3 et 255 caractères !';
+$_['error_author_found']     = 'Ce nom d\'auteur existe deja, veuillez entrer un nom unique !';
+$_['error_article']          = 'Attention : cet auteur ne peut être supprimé car il est actuellement attribué à %s articles !';
