7a5d276ab20f2f55de3e88c9d47fa7796eb49ad2
Philippe Marion <contact@nivocart.com>
2019-01-20 15:22:30 +0000

    New translations category.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/catalog/category.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']          = 'Catégorie';
+
+// Text
+$_['text_success']           = 'Vous avez modifié les <b>catégories</b> avec succès !';
+$_['text_default']           = 'Par défaut';
+$_['text_image_manager']     = 'Gestionnaire de fichiers';
+$_['text_browse']            = 'Parcourir';
+$_['text_clear']             = 'Effacer';
+$_['text_autocomplete']      = '<span class="help">(Auto-complétion)</span>';
+
+// Column Form
+$_['column_store']           = 'Boutique';
+$_['column_layout']          = 'Override de mise en page';
+
+// Column List
+$_['column_id']              = 'ID';
+$_['column_image']           = 'Image';
+$_['column_name']            = 'Nom de la catégorie';
+$_['column_sort_order']      = 'Classement';
+$_['column_status']          = 'État ';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Nom de la catégorie :';
+$_['entry_meta_description'] = 'Meta Tag Description :';
+$_['entry_meta_keyword']     = 'Meta Tag Keywords :';
+$_['entry_description']      = 'Description :';
+$_['entry_parent']           = 'Parent :';
+$_['entry_filter']           = 'Filtres :';
+$_['entry_store']            = 'Boutiques :';
+$_['entry_keyword']          = 'Mot-clé SEO :';
+$_['entry_image']            = 'Image :';
+$_['entry_top']              = 'Haut :';
+$_['entry_column']           = 'Colonnes :';
+$_['entry_sort_order']       = 'Classement : ';
+$_['entry_status']           = 'État :';
+$_['entry_layout']           = 'Override de mise en page :';
+
+// Help
+$_['help_meta_description']  = 'Le nombre optimal de caractères est environ 155, y compris les espaces.<br />SEO importance : haute.';
+$_['help_meta_keyword']      = 'Généralement ignorés par les moteurs de recherche.<br />SEO importance : faible.';
+$_['help_keyword']           = 'N\'utilisez pas d\'espaces, mais remplacez-les par des "-" et assurez-vous que le mot-clef soit absolument unique.';
+$_['help_top']               = 'Afficher dans la barre de menu du haut. Ne fonctionne que pour les catégories parentes.';
+$_['help_column']            = 'Nombre de colonnes à utiliser pour les catégories bas 3. Ne fonctionne que pour les catégories parentes.';
+
+// Error
+$_['error_permission']       = 'Attention : vous n’êtes pas autorisé à modifier les <b>catégories</b> !';
+$_['error_warning']          = 'Attention: veuillez vérifier si le formulaire ne contient pas d\'erreurs !';
+$_['error_name']             = 'Le nom de la catégorie doit contenir entre 2 et 32 caractères !';
+$_['error_image']            = 'L\'image doit être dans un format valide!';
