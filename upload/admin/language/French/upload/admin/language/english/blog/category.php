9bfea74ffe9c3dfcc4dfa97e6c7bc9a905c2958a
Philippe Marion <contact@nivocart.com>
2019-01-20 15:22:24 +0000

    New translations category.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/blog/category.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']          = 'Catégorie de blog';
+
+// Text
+$_['text_success']           = 'Succès : Vous avez modifié des catégories du Blog !';
+$_['text_default']           = 'Par défaut';
+$_['text_image_manager']     = 'Gestionnaire de fichiers';
+$_['text_browse']            = 'Parcourir les fichiers';
+$_['text_clear']             = 'Effacer l\'image';
+
+// Column
+$_['column_name']            = 'Nom de la catégorie';
+$_['column_sort_order']      = 'Classement';
+$_['column_status']          = 'État ';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Nom de la catégorie :';
+$_['entry_meta_description'] = 'Meta Tag Description :<span class="help">le nombre optimal de caractères est environ 155, y compris les espaces.<br />SEO importance : haute.</span>';
+$_['entry_meta_keyword']     = 'Mot clé Meta :<span class="help">généralement ignorés par les moteurs de recherche.<br />SEO importance : faible.</span>';
+$_['entry_description']      = 'Description :';
+$_['entry_parent']           = 'Catégorie parente :';
+$_['entry_store']            = 'Boutiques :';
+$_['entry_keyword']          = 'Mot-clef SEO:<span class="help">N\'utilisez pas d\'espaces, mais remplacez-les par des "-" et assurez-vous que le mot-clef soit absolument unique.</span>';
+$_['entry_image']            = 'Image :';
+$_['entry_article_limit']    = 'Limite d’article par page :<span class="help">nombre d’articles affichés sur la page de cette catégorie.</span>';
+$_['entry_sort_order']       = 'Classement : ';
+$_['entry_status']           = 'État :';
+$_['entry_layout']           = 'Override de mise en page :';
+
+// Error
+$_['error_warning']          = 'Attention: veuillez vérifier si le formulaire ne contient pas d\'erreurs !';
+$_['error_permission']       = 'Attention : vous n’êtes pas autorisé à modifier les catégories !';
+$_['error_name']             = 'Le nom de la catégorie doit contenir entre 2 et 255 caractères !';
+$_['error_article']          = 'Attention : cet Catégorie ne peut être supprimée car elle est actuellement attribuée à %s articles !';
