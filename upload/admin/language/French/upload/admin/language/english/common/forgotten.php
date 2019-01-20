ca34a9e6d8375935059b9cd82b53e6d16c22226d
Philippe Marion <contact@nivocart.com>
2019-01-20 15:22:20 +0000

    New translations forgotten.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/common/forgotten.php
@@ -0,0 +1,20 @@
+<?php
+// header
+$_['heading_title']   = 'Mot de passe oublié ?';
+
+// Text
+$_['text_forgotten']  = 'Mot de passe oublié';
+$_['text_your_email'] = 'Votre Adresse Email';
+$_['text_email']      = 'Entrez l’adresse Email associée à votre compte. Cliquez sur réinitialiser pour avoir un lien de réinitialisation de mot de passe envoyé à vous.';
+$_['text_success']    = 'Un email contenant le lien de vérification a été envoyé à votre adresse email.';
+
+// Entry
+$_['entry_email']     = 'Votre Adresse Email:';
+$_['entry_password']  = 'Nouveau mot de passe:';
+$_['entry_confirm']   = 'Confirmer:';
+
+// Error
+$_['error_email']     = 'L`adresse Email ne semble pas être valide!';
+$_['error_record']    = 'Adresse Email est introuvable dans nos dossiers, veuillez essayer à nouveau !';
+$_['error_password']  = 'Password must be between 3 and 20 characters!';
+$_['error_confirm']   = 'Le mot de passe et la confirmation du mot de passe ne correspondent pas!';
