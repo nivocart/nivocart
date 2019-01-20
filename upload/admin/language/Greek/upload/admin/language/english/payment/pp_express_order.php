5a42e0e36b8a1e618d1d645c56caa6e68b198174
Philippe Marion <contact@nivocart.com>
2019-01-20 16:31:43 +0000

    New translations pp_express_order.php (Greek)

---
+++ upload/admin/language/Greek/upload/admin/language/english/payment/pp_express_order.php
@@ -0,0 +1,40 @@
+<?php
+// Text
+$_['text_payment_info']          = 'Payment Information';
+$_['text_capture_status']        = 'Capture status';
+$_['text_amount_authorised']     = 'Amount authorised';
+$_['text_amount_captured']       = 'Amount captured';
+$_['text_amount_refunded']       = 'Amount refunded';
+$_['text_transactions']          = 'Transactions';
+$_['text_complete']              = 'Complete';
+$_['text_confirm_void']          = 'If you void you cannot capture any further funds';
+$_['text_success']               = 'Transaction was successfully sent';
+
+// Column
+$_['column_transaction_id']      = 'Transaction ID';
+$_['column_amount']              = 'Amount';
+$_['column_type']                = 'Payment Type';
+$_['column_status']              = 'Status';
+$_['column_pending_reason']      = 'Pending Reason';
+$_['column_created']             = 'Created';
+$_['column_actions']             = 'Actions';
+
+// Entry
+$_['entry_capture_amount']       = 'Capture amount';
+$_['entry_capture_complete']     = 'Complete capture';
+
+// Button
+$_['button_void']                = 'Void';
+$_['button_capture']             = 'Capture';
+$_['button_view']                = 'View';
+$_['button_refund']              = 'Refund';
+$_['button_resend']              = 'Resend';
+
+// Message
+$_['success_transaction_resent'] = 'Transaction was successfully resent';
+
+// Error
+$_['error_capture_amt']          = 'Enter a positive amount to capture!';
+$_['error_timeout']              = 'Request timed out!';
+$_['error_transaction_missing']  = 'Transaction could not be found!';
+$_['error_partial_amt']          = 'You must enter a partial refund amount!';
