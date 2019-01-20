9b3fe189f090fcccb29c50f525954fcf20cd4954
Philippe Marion <contact@nivocart.com>
2019-01-20 16:43:45 +0000

    New translations html.php (Hindi)

---
+++ upload/admin/language/Hindi/upload/admin/language/english/module/html.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']         = 'HTML';
+
+// Text
+$_['text_module']           = 'Modules';
+$_['text_success']          = 'Success: You have modified <b>HTML</b> !';
+
+// Tab
+$_['tab_code']              = 'Code';
+
+// Entry
+$_['entry_theme']           = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']           = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+$_['entry_code']            = 'HTML Code:<span class="help">Enter any content directly in the textarea.</span>';
+
+$_['entry_tab_id']          = 'Code';
+$_['entry_layout']          = 'Layout';
+$_['entry_position']        = 'Position';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify <b>HTML</b> !';
