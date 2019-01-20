a2431f28f6b11d3f9c98ea0139baa3c22408fa43
Philippe Marion <contact@nivocart.com>
2019-01-20 15:22:17 +0000

    New translations not_found.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/error/not_found.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title']  = 'Page non trouvée !';
+
+// Text
+$_['text_not_found'] = 'The page you are looking for could not be found! Please refer to your System Administrator.';
