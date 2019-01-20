569dd28b37996667489851e6e1127f7802dbb863
Philippe Marion <contact@nivocart.com>
2019-01-20 15:22:29 +0000

    New translations attribute_group.php (French)

---
+++ upload/admin/language/French/upload/admin/language/english/catalog/attribute_group.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']     = 'Groupes d’attributs';
+
+// Text
+$_['text_success']      = 'Vous avez modifié les <b>Groupes d\'Attributs</b> avec succès !';
+
+// Column
+$_['column_name']       = 'Nom du groupe d’attributs';
+$_['column_sort_order'] = 'Classement';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_name']        = 'Nom du groupe d’attributs:';
+$_['entry_sort_order']  = 'Classement:';
+
+// Error
+$_['error_permission']  = 'Attention : Vous n\'êtes pas autorisé à modifier les <b>Groupes d\'Attributs</b> !';
+$_['error_name']        = 'Le nom du groupe d’attributs doit contenir entre 3 et 64 caractères !';
+$_['error_attribute']   = 'Attention : Ce groupe d\'attributs ne peut être supprimé car il est actuellement affecté à %s attributs !';
+$_['error_product']     = 'Attention : Ce groupe d\'attributs ne peut être supprimé car il est actuellement affecté à %s produits !';
