4b771ed76fc7337a97bd9ab61b9d1b850f70feaf
Philippe Marion <contact@nivocart.com>
2019-01-23 10:08:14 +0000

    New translations footer.php (Indonesian)

---
+++ upload/admin/language/Indonesian/upload/admin/language/english/design/footer.php
@@ -0,0 +1,46 @@
+<?php
+// Heading
+$_['heading_title']       = 'Footers';
+$_['heading_block']       = 'Block Items';
+
+// Text
+$_['text_success']        = 'Success: You have modified <b>Footers</b> !';
+$_['text_default']        = 'Default';
+$_['text_position']       = 'Position';
+$_['text_block']          = 'Block';
+$_['text_link']           = 'Internal Links:<span class="help">External links require a complete Url,<br />including the "http://".</span>';
+$_['text_info']           = 'Click <a class="colorbox" href="%s" title="List of internal links"><b>Here</b></a> to see a list of internal links.';
+$_['text_footer_info']    = 'The Footer Manager allows you to create blocks of links for your frontend Footer.<br />
+Each Footer Block must be allocated to a "Block Position", which is a column from 1 to 4, from left to right.<br />
+Each Footer Block also has a "Sort Order" option, so multiple blocks can be placed in the same "Block Position".<br />
+Footer Blocks are just passive lists of internal (or external) links. They require a frontend theme to be styled and displayed.<br />
+To create a new Footer Block, simply click the "Insert" button above. To change something in a block, just click "Edit".';
+
+// Column
+$_['column_name']         = 'Block Name';
+$_['column_position']     = 'Block Position';
+$_['column_status']       = 'Status';
+$_['column_action']       = 'Action';
+
+// Entry
+$_['entry_name']          = 'Block Name:';
+$_['entry_position']      = 'Block Position:<span class="help">Specify the footer column where this block is to appear. Blocks 1 to 4, from left to right.</span>';
+$_['entry_store']         = 'Stores:';
+$_['entry_status']        = 'Status:';
+
+$_['entry_title']         = 'Link Title';
+$_['entry_route']         = 'Address (Url)';
+$_['entry_external_link'] = 'External Link';
+$_['entry_sort_order']    = 'Sort Order';
+
+// Info
+$_['info_title']          = 'List of internal links';
+
+// Button
+$_['button_info']         = 'Information';
+$_['button_theme']        = 'Themes';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify <b>Footers</b> !';
+$_['error_name']          = 'Footer Name must be between 3 and 64 characters!';
+$_['error_title']         = 'Footer Title must be between 3 and 64 characters!';
