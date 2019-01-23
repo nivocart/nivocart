0e059c0be48e9fe737fdeff5988402b3c765121e
Philippe Marion <contact@nivocart.com>
2019-01-23 18:38:39 +0000

    New translations google_hangouts.php (Lithuanian)

---
+++ upload/admin/language/Lithuanian/upload/admin/language/english/module/google_hangouts.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']       = 'Google Hangouts';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module <b>Google Hangouts</b> !';
+
+// Entry
+$_['entry_theme']         = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']         = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+$_['entry_code']          = 'Google Hangout Code:<br /><span class="help">Go to <a onclick="window.open(\'https://developers.google.com/+/hangouts/button\');"><u>Create a Google Hangout chatback badge</u></a> and copy &amp; paste the generated code into the text box.</span>';
+
+$_['entry_layout']        = 'Layout';
+$_['entry_position']      = 'Position';
+$_['entry_status']        = 'Status';
+$_['entry_sort_order']    = 'Sort Order';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module <b>Google Hangouts</b> !';
+$_['error_code']          = 'Google Code is Required!';
