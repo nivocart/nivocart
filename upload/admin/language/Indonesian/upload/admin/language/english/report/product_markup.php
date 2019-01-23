28abac4fcc9e878b06706dd9530a8185c2eb3315
Philippe Marion <contact@nivocart.com>
2019-01-23 10:08:52 +0000

    New translations product_markup.php (Indonesian)

---
+++ upload/admin/language/Indonesian/upload/admin/language/english/report/product_markup.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']      = 'Product Profit Margins Report';
+
+// Text
+$_['text_free_products'] = 'Please note that products with a zero price (Free) will not show in this report. Prices in <span style="color:#E53030; font-weight:bold;">RED</span> are special offer prices.';
+
+// Column
+$_['column_product_id']  = 'ID';
+$_['column_name']        = 'Name';
+$_['column_price']       = 'Price';
+$_['column_cost']        = 'Cost';
+$_['column_ratio']       = 'Ratio';
+$_['column_graph']       = 'Graph';
