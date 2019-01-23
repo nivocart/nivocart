1c92250ca656de8e6d1fb4782fea51ae9e1a43e0
Philippe Marion <contact@nivocart.com>
2019-01-23 10:20:23 +0000

    New translations success.php (Italian)

---
+++ upload/catalog/language/Italian/upload/catalog/language/english/affiliate/success.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'Your Affiliate Account Has Been Created!';
+
+// Text
+$_['text_message']  = '<p>Congratulations! Your new account has been successfully created!</p> <p>You are now a member of %s affiliates.</p> <p>If you have ANY questions about the operation of this affiliate system, please Email the store administrator.</p> <p>A confirmation has been sent to the provided Email Address. If you have not received it within the hour, please <a href="%s">contact us</a>.</p>';
+$_['text_approval'] = '<p>Thank you for registering for an affiliate account with %s!</p><p>You will be notified by Email once your account has been activated by the store administrator.</p><p>If you have ANY questions about the operation of this affiliate system, please <a href="%s">contact the store administrator</a>.</p>';
+$_['text_account']  = 'Account';
+$_['text_success']  = 'Success';
