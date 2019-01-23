69e589b530b8ea4e9b1e4497354cb0cac4290a5f
Philippe Marion <contact@nivocart.com>
2019-01-23 10:11:15 +0000

    New translations store.php (Indonesian)

---
+++ upload/catalog/language/Indonesian/upload/catalog/language/english/module/store.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']     = 'Store';
+
+// Text
+$_['text_selector']     = 'Select a Store';
+$_['text_default']      = 'Home Store';
+
+// Buttons
+$_['button_adminlogin'] = 'Admin Login';
