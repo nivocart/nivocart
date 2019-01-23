3899f2dba019cce8b6c627b4b0b7c2e55d9359b7
Philippe Marion <contact@nivocart.com>
2019-01-23 10:10:06 +0000

    New translations tagcloud.php (Indonesian)

---
+++ upload/admin/language/Indonesian/upload/admin/language/english/module/tagcloud.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']         = 'Tag Cloud';
+
+// Text
+$_['text_module']           = 'Modules';
+$_['text_success']          = 'Success: You have modified module <b>Tag Cloud</b> !';
+$_['text_normal']           = 'Normal';
+$_['text_bold']             = 'Bold';
+$_['text_limit']            = 'N&deg; of Tags:';
+
+// Entry
+$_['entry_theme']           = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']           = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+
+$_['entry_random']          = 'Random';
+$_['entry_limit']           = 'Limit';
+$_['entry_min_font_size']   = 'Min Size';
+$_['entry_max_font_size']   = 'Max Size';
+$_['entry_font_weight']     = 'Font Weight';
+$_['entry_layout']          = 'Layout';
+$_['entry_position']        = 'Position';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify module <b>Tag Cloud</b> !';
+$_['error_limit']           = 'Limit is required!';
+$_['error_min_font_size']   = 'Minimum font size is required and must be at least 9px!';
+$_['error_max_font_size']   = 'Maximum font size is required and must be below 37px!';
