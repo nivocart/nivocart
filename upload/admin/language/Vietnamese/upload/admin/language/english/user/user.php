df1bbdcbe268b9275835c0db23411a1c450761e6
Philippe Marion <contact@nivocart.com>
2019-01-24 18:39:28 +0000

    New translations user.php (Vietnamese)

---
+++ upload/admin/language/Vietnamese/upload/admin/language/english/user/user.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['heading_title']           = 'User';
+
+// Text
+$_['text_success']            = 'Success: You have modified <b>Users</b> !';
+$_['text_image_manager']      = 'File Manager';
+$_['text_browse']             = 'Browse';
+$_['text_clear']              = 'Clear';
+
+// Column
+$_['column_user_id']          = 'ID';
+$_['column_avatar']           = 'Avatar';
+$_['column_username']         = 'Username';
+$_['column_user_group']       = 'Group';
+$_['column_email']            = 'Email';
+$_['column_date_added']       = 'Date Added';
+$_['column_status']           = 'Status';
+$_['column_action']           = 'Action';
+
+// Entry
+$_['entry_username']          = 'Username:';
+$_['entry_old_password']      = 'Current Password:';
+$_['entry_password']          = 'New Password:';
+$_['entry_confirm']           = 'Confirm Password:';
+$_['entry_firstname']         = 'First Name:';
+$_['entry_lastname']          = 'Last Name:';
+$_['entry_email']             = 'Email:';
+$_['entry_image']             = 'Avatar:';
+$_['entry_user_group']        = 'User Group:';
+$_['entry_status']            = 'Status:';
+
+// Help
+$_['help_image']              = 'Clearing the image cache may be required before replacing the avatar image.';
+
+// Error
+$_['error_permission']        = 'Warning: You do not have permission to modify <b>Users</b> !';
+$_['error_account']           = 'Warning: You can not delete your own account!';
+$_['error_exists']            = 'Warning: Username is already in use!';
+$_['error_email']             = 'Email Address does not appear to be valid!';
+$_['error_email_exists']      = 'Warning: Email Address is already in use!';
+$_['error_username']          = 'Username must be between 3 and 20 characters!';
+$_['error_syntax']            = 'Warning: Username should only contain alphanumeric characters!';
+$_['error_old_password']      = 'Current Password is required and does not match our records!';
+$_['error_password']          = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']           = 'Password and password confirmation do not match!';
+$_['error_firstname']         = 'First Name must be between 1 and 32 characters!';
+$_['error_lastname']          = 'Last Name must be between 1 and 32 characters!';
