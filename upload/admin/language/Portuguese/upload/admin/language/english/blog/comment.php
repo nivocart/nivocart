a539b1e2541e6636099e2f27ed66e8719d8b6140
Philippe Marion <contact@nivocart.com>
2019-01-24 18:01:58 +0000

    New translations comment.php (Portuguese)

---
+++ upload/admin/language/Portuguese/upload/admin/language/english/blog/comment.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Blog Comment';
+
+// Text
+$_['text_success']                  = 'Success: You have modified Blog Comments!';
+
+// Tab
+$_['tab_comment']                   = 'Comment';
+
+// Column
+$_['column_article_title']          = 'Article Title';
+$_['column_author_name']            = 'Author Name';
+$_['column_status']                 = 'Status';
+$_['column_date_added']             = 'Date Added';
+$_['column_action']                 = 'Action';
+
+// Entry
+$_['entry_author']                  = 'Author Name:';
+$_['entry_article']                 = 'Article Name:<span class="help">(Autocomplete)</span>';
+$_['entry_status']                  = 'Status:';
+$_['entry_comment']                 = 'Comment:';
+$_['entry_reply_comment']           = 'Reply:';
+
+// Button
+$_['button_add_reply']              = 'Add Comment Reply';
+
+// Error
+$_['error_warning']                 = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']              = 'Warning: You do not have permission to modify comment!';
+$_['error_author']                  = 'Author name must be between 3 to 63 characters!';
+$_['error_comment']                 = 'Comment must be between 3 to 1000 characters!';
+$_['error_article_title']           = 'Article Title is required!';
+$_['error_article_title_not_found'] = 'Article Title not found!';
