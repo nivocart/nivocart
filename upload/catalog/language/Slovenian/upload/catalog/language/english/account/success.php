9256833a5ea51dd5d931b17e1ba9d92d1d1d0314
Philippe Marion <contact@nivocart.com>
2019-01-24 18:15:03 +0000

    New translations success.php (Slovenian)

---
+++ upload/catalog/language/Slovenian/upload/catalog/language/english/account/success.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'Your Account Has Been Created!';
+
+// Text
+$_['text_message']  = '<p>Congratulations! Your new account has been successfully activated!</p> <p>You can now take advantage of member privileges to enhance your online shopping experience with us.</p> <p>If you have any questions about the operation of this online store, please send us an email.</p> <p>A confirmation has been sent to the provided email address. If you have not received it within the hour, please <a href="%s">contact us</a>.</p>';
+$_['text_approval'] = '<p>Thank you for registering with %s!</p> <p>You will be notified by email once your account has been activated by our customer service department.</p> <p>If you have any questions about the operation of this online store, please <a href="%s">send us an email</a>.</p>';
+$_['text_account']  = 'Account';
+$_['text_success']  = 'Success';
