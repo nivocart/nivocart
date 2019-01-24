6ed9dad3e39fe32f9c0ec0635e57221b96682d6a
Philippe Marion <contact@nivocart.com>
2019-01-24 18:22:32 +0000

    New translations palette.php (Swedish)

---
+++ upload/admin/language/Swedish/upload/admin/language/english/module/palette.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']        = 'Palette';
+
+// Text
+$_['text_module']          = 'Modules';
+$_['text_success']         = 'Success: You have modified module <b>Palette</b> !';
+$_['text_manage_palettes'] = 'Add, Edit or View, Palettes.';
+$_['text_manage_products'] = 'Add, Edit or View, Palette colors in Products.';
+
+// Entry
+$_['entry_theme']          = 'Complete Module:<span class="help">Show content with header.</span>';
+$_['entry_title']          = 'Custom Title:<span class="help">Overrides the default module title.</span>';
+
+$_['entry_layout']         = 'Layout';
+$_['entry_position']       = 'Position';
+$_['entry_status']         = 'Status';
+$_['entry_sort_order']     = 'Sort Order';
+
+// Button
+$_['button_palette']       = 'Palettes';
+$_['button_product']       = 'Products';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to modify module <b>Palette</b> !';
