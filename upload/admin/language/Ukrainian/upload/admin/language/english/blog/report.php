b538c3a3c8f910488c47d9261d95a680cf3fed22
Philippe Marion <contact@nivocart.com>
2019-01-24 18:28:39 +0000

    New translations report.php (Ukrainian)

---
+++ upload/admin/language/Ukrainian/upload/admin/language/english/blog/report.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']       = 'Blog Viewed Report';
+
+// Text
+$_['text_success']        = 'Success: You have modified Blog Viewed report!';
+
+// Column
+$_['column_article_name'] = 'Article Name';
+$_['column_author_name']  = 'Author Name';
+$_['column_viewed']       = 'Viewed';
+$_['column_percent']      = 'Percent';
+
+// Entry
+$_['entry_date_start']    = 'Date Start:';
+$_['entry_date_end']      = 'Date End:';
