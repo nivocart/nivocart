15b2438457f9b4d61709a28dec880664adc8edc7
Philippe Marion <contact@nivocart.com>
2019-08-18 10:37:03 +0100

    PDF downloads errors #18
    DomPDF functions updates

--- upload/catalog/controller/account/order.php
+++ upload/catalog/controller/account/order.php
@@ -718,7 +718,7 @@ class ControllerAccountOrder extends Controller {
 		$this->data['column_tax_percent'] = $this->language->get('column_tax_percent');
 		$this->data['column_total'] = $this->language->get('column_total');
 
-		$this->data['token'] = $this->request->get['token'];
+		$this->data['token'] = $this->customer->setToken();
 
 		// Get tax breakdown
 		if ($this->config->get('config_tax_breakdown')) {
