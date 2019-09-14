ac2f4e0e48f7ac97da317ba4f6dd78c896a62390
Philippe Marion <contact@nivocart.com>
2019-09-14 15:10:59 +0100

    New translations category.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/catalog/category.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/catalog/category.php
@@ -8,34 +8,34 @@ $_['text_default']           = 'Default';
 $_['text_image_manager']     = 'File Manager';
 $_['text_browse']            = 'Browse';
 $_['text_clear']             = 'Clear';
-$_['text_autocomplete']      = '<span class="help">(Autocomplete)</span>';
+$_['text_autocomplete']      = '<span class="help">(самодопълващо се)</span>';
 
 // Column Form
-$_['column_store']           = 'Store';
+$_['column_store']           = 'Магазин';
 $_['column_layout']          = 'Layout Override';
 
 // Column List
-$_['column_id']              = 'ID';
-$_['column_image']           = 'Image';
-$_['column_name']            = 'Category Name';
+$_['column_id']              = 'Ид. №';
+$_['column_image']           = 'Изображение';
+$_['column_name']            = 'Име на категория';
 $_['column_sort_order']      = 'Sort Order';
-$_['column_status']          = 'Status';
-$_['column_action']          = 'Action';
+$_['column_status']          = 'Състояние';
+$_['column_action']          = 'Действие';
 
 // Entry
-$_['entry_name']             = 'Category Name:';
+$_['entry_name']             = 'Име на категория:';
 $_['entry_meta_description'] = 'Meta Tag Description:';
 $_['entry_meta_keyword']     = 'Meta Tag Keywords:';
-$_['entry_description']      = 'Description:';
-$_['entry_parent']           = 'Parent:';
-$_['entry_filter']           = 'Filters:';
-$_['entry_store']            = 'Stores:';
+$_['entry_description']      = 'Описание:';
+$_['entry_parent']           = 'Родител:';
+$_['entry_filter']           = 'Филтри:';
+$_['entry_store']            = 'Магазини:';
 $_['entry_keyword']          = 'SEO Keyword:';
-$_['entry_image']            = 'Image:';
+$_['entry_image']            = 'Изображение:';
 $_['entry_top']              = 'Top:';
-$_['entry_column']           = 'Columns:';
+$_['entry_column']           = 'Колони:';
 $_['entry_sort_order']       = 'Sort Order:';
-$_['entry_status']           = 'Status:';
+$_['entry_status']           = 'Състояние:';
 $_['entry_layout']           = 'Layout Override:';
 
 // Help
@@ -49,4 +49,4 @@ $_['help_column']            = 'Number of columns to use for the bottom 3 catego
 $_['error_permission']       = 'Warning: You do not have permission to modify <b>Categories</b> !';
 $_['error_warning']          = 'Warning: Please check the form carefully for errors!';
 $_['error_name']             = 'Category Name must be between 2 and 32 characters!';
-$_['error_image']            = 'Image must be a valid image format!';
+$_['error_image']            = 'Изображението трябва да е във валиден формат!';
