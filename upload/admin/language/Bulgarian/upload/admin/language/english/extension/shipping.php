46490e6b7addc59f4bb1171b5f73b81931351083
Philippe Marion <contact@nivocart.com>
2019-09-14 16:13:52 +0100

    New translations shipping.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/extension/shipping.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/extension/shipping.php
@@ -3,14 +3,14 @@
 $_['heading_title']     = 'Shipping';
 
 // Text
-$_['text_install']      = 'Install';
-$_['text_uninstall']    = 'Uninstall';
+$_['text_install']      = 'Инсталиране';
+$_['text_uninstall']    = 'Деинсталиране';
 
 // Column
 $_['column_name']       = 'Shipping Method';
-$_['column_status']     = 'Status';
+$_['column_status']     = 'Състояние';
 $_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_action']     = 'Действие';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify <b>Shipping</b> !';
