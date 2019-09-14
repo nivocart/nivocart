ea0f8160514b4926247edaf5f954df4cde70f543
Philippe Marion <contact@nivocart.com>
2019-09-14 16:14:00 +0100

    New translations geo_zone.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/localisation/geo_zone.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/localisation/geo_zone.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']      = 'Geo Zones';
+$_['heading_title']      = 'Гео зони';
 
 // Text
 $_['text_success']       = 'Success: You have modified <b>Geo Zones</b> !';
