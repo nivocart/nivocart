f8cf0739c4b2b422f6d28f2ca7b577bdb93e463a
Philippe Marion <contact@nivocart.com>
2019-09-14 16:13:55 +0100

    New translations storeya.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/feed/storeya.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/feed/storeya.php
@@ -8,10 +8,10 @@ $_['text_success']     = 'Success: You have modified <b>StoreYa</b> feed!';
 $_['text_link']        = '<span class="help">Click to go to the StoreYa page, choose a plan and then paste the link in the "Data Feed Url" field above!</span>';
 
 // Entry
-$_['entry_status']     = 'Status:';
+$_['entry_status']     = 'Състояние:';
 $_['entry_limits']     = 'Product Limit:';
-$_['entry_currency']   = 'Currency';
-$_['entry_language']   = 'Language';
+$_['entry_currency']   = 'Валута';
+$_['entry_language']   = 'Език';
 $_['entry_data_feed']  = 'Data Feed Url:';
 
 // Error
