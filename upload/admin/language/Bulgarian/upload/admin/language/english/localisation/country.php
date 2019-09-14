3bc9d32323aea7e05dca7ae0843fb9114100e6d5
Philippe Marion <contact@nivocart.com>
2019-09-14 16:13:59 +0100

    New translations country.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/localisation/country.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/localisation/country.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']           = 'Country';
+$_['heading_title']           = 'Държава';
 
 // Text
 $_['text_success']            = 'Success: You have modified <b>Countries</b> !';
