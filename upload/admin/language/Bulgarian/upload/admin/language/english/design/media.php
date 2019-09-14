ff87613e0b7cdad07c62cb30256351fb3ccd7dd8
Philippe Marion <contact@nivocart.com>
2019-09-14 15:54:32 +0100

    New translations media.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/design/media.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/design/media.php
@@ -1,30 +1,30 @@
 <?php
 // Heading
-$_['heading_title']        = 'Medias';
+$_['heading_title']        = 'Медии';
 
 // Text
 $_['text_success']         = 'Success: You have modified <b>Medias</b> !';
-$_['text_default']         = 'Default';
-$_['text_image_manager']   = 'File Manager';
-$_['text_browse']          = 'Browse';
-$_['text_clear']           = 'Clear';
+$_['text_default']         = 'По подразбиране';
+$_['text_image_manager']   = 'Управител на файлове';
+$_['text_browse']          = 'Разглеждане';
+$_['text_clear']           = 'Изчистване';
 
 // Column
-$_['column_name']          = 'Name';
-$_['column_media']         = 'Media';
-$_['column_size']          = 'Size';
+$_['column_name']          = 'Име';
+$_['column_media']         = 'Медия';
+$_['column_size']          = 'Големина';
 $_['column_type']          = 'Type';
-$_['column_status']        = 'Status';
-$_['column_action']        = 'Action';
+$_['column_status']        = 'Състояние';
+$_['column_action']        = 'Действие';
 
 // Entry
 $_['entry_name']           = 'Media Name:';
 $_['entry_media']          = 'Media File:<span class="help">Supported formats: mp3, mp4, oga, ogv, ogg, webm, m4a, m4v, wma, wmv and flv.</span>';
 $_['entry_credit']         = 'Credits:<span class="help">Media content may be subject to copyrights.</span>';
-$_['entry_status']         = 'Status:';
+$_['entry_status']         = 'Състояние:';
 
 // Button
-$_['button_module']        = 'Module';
+$_['button_module']        = 'Модул';
 
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify <b>Medias</b> !';
