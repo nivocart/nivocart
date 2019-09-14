6d51aa4776e8b4081ae85be68ba28b0e704d7ff0
Philippe Marion <contact@nivocart.com>
2019-09-14 16:13:53 +0100

    New translations theme.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/extension/theme.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/extension/theme.php
@@ -1,10 +1,10 @@
 <?php
 // Heading
-$_['heading_title']    = 'Themes';
+$_['heading_title']    = 'Теми';
 
 // Text
-$_['text_install']     = 'Install';
-$_['text_uninstall']   = 'Uninstall';
+$_['text_install']     = 'Инсталиране';
+$_['text_uninstall']   = 'Деинсталиране';
 $_['text_is_active']   = '<b>(Active)</b>';
 
 // Column
@@ -12,7 +12,7 @@ $_['column_name']      = 'Theme Name';
 $_['column_action']    = 'Action';
 
 // Button
-$_['button_settings']  = 'Settings';
+$_['button_settings']  = 'Настройки';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify <b>Themes</b> !';
