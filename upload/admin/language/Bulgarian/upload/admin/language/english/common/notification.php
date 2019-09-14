49f548e59f06194629371355c18f62aac6ab80a5
Philippe Marion <contact@nivocart.com>
2019-09-14 15:51:03 +0100

    New translations notification.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/common/notification.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/common/notification.php
@@ -1,15 +1,15 @@
 <?php
 // Text
-$_['text_notification']    = 'Notifications';
+$_['text_notification']    = 'Известия';
 $_['text_order']           = 'Orders';
 $_['text_pending_status']  = 'Pending';
-$_['text_complete_status'] = 'Complete';
-$_['text_return']          = 'Returns';
-$_['text_customer']        = 'Customers';
+$_['text_complete_status'] = 'Завършено';
+$_['text_return']          = 'Връщания';
+$_['text_customer']        = 'Клиенти';
 $_['text_online']          = 'Customers online';
 $_['text_deleted']         = 'Deleted accounts';
 $_['text_approval']        = 'Pending approval';
-$_['text_product']         = 'Products';
+$_['text_product']         = 'Продукти';
 $_['text_stock']           = 'Out of stock';
 $_['text_low_stock']       = 'Low stock';
 $_['text_review']          = 'Reviews';
