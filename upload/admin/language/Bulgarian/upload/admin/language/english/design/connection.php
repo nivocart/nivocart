7710c6faf9e24f85775bd303b8281db0db4d7aee
Philippe Marion <contact@nivocart.com>
2019-09-14 15:51:06 +0100

    New translations connection.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/design/connection.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/design/connection.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']    = 'Links';
+$_['heading_title']    = 'Връзки';
 
 // Text
 $_['text_success']     = 'Success: You have modified <b>Links</b> !';
-$_['text_default']     = 'Default';
+$_['text_default']     = 'По подразбиране';
 
 // Column
-$_['column_name']      = 'Link Name';
-$_['column_backend']   = 'Administration';
+$_['column_name']      = 'Име на връзката';
+$_['column_backend']   = 'Администрация';
 $_['column_frontend']  = 'Store Front';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Действие';
 
 // Entry
-$_['entry_name']       = 'Link Name:';
-$_['entry_backend']    = 'Administration (Admin):';
+$_['entry_name']       = 'Име на връзката:';
+$_['entry_backend']    = 'Администрация (админ):';
 $_['entry_frontend']   = 'Store Front (Catalog):';
-$_['entry_icon']       = 'Icon';
-$_['entry_title']      = 'Title';
+$_['entry_icon']       = 'Икона';
+$_['entry_title']      = 'Заглавие';
 $_['entry_route']      = 'Address (Url)';
 
 // Button
