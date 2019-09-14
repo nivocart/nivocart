de8c3836bef83264ff443186fdc9c305ec43b0fa
Philippe Marion <contact@nivocart.com>
2019-09-14 14:50:56 +0100

    New translations article.php (Bulgarian)

--- upload/admin/language/Bulgarian/upload/admin/language/english/blog/article.php
+++ upload/admin/language/Bulgarian/upload/admin/language/english/blog/article.php
@@ -5,47 +5,47 @@ $_['heading_title']                = 'Blog Article';
 // Text
 $_['text_success']                 = 'Success: You have modified Blog articles!';
 $_['text_browse']                  = 'Browse';
-$_['text_clear']                   = 'Clear';
+$_['text_clear']                   = 'Изчистване';
 $_['text_default']                 = 'Default';
-$_['text_image_manager']           = 'File Manager';
+$_['text_image_manager']           = 'Управител на файлове';
 
 // Button
-$_['button_add_description']       = 'Add Description';
-$_['button_add_articles']          = 'Add Article';
+$_['button_add_description']       = 'Добавяне на описание';
+$_['button_add_articles']          = 'Добавяне на статия';
 
 // Column
-$_['column_article_title']         = 'Article Title';
-$_['column_author_name']           = 'Author Name';
+$_['column_article_title']         = 'Заглавие на статия';
+$_['column_author_name']           = 'Име на автора';
 $_['column_sort_order']            = 'Sort Order';
 $_['column_status']                = 'Status';
-$_['column_date_added']            = 'Date Added';
-$_['column_action']                = 'Action';
+$_['column_date_added']            = 'Дата на добавяне';
+$_['column_action']                = 'Действие';
 
 // Tab
-$_['tab_data']                     = 'Content';
-$_['tab_related']                  = 'Related';
-$_['tab_design']                   = 'Design';
+$_['tab_data']                     = 'Съдържание';
+$_['tab_related']                  = 'Сродни';
+$_['tab_design']                   = 'Външен вид';
 
 // Entry
 $_['entry_title']                  = 'Article Title:<span class="help">Article Title must be unique.</span>';
 $_['entry_author_name']            = 'Author Name:<span class="help">(Autocomplete)</span>';
 $_['entry_meta_description']       = 'Meta Description:<span class="help">The optimal number of characters is around 155, including spaces.<br />SEO importance: High.</span>';
 $_['entry_meta_keyword']           = 'Meta keyword:<span class="help">Usually ignored by search engines.<br />SEO importance: Low.</span>';
-$_['entry_description']            = 'Description:';
+$_['entry_description']            = 'Описание:';
 $_['entry_allow_comment']          = 'Allow comments for this article:';
 $_['entry_keyword']                = 'SEO Keyword:<span class="help">Do not use spaces instead replace spaces with - and make sure the keyword is globally unique.</span>';
 $_['entry_image']                  = 'Image:<span class="help">Main article image.</span>';
 $_['entry_sort_order']             = 'Sort Order:';
 $_['entry_status']                 = 'Status:';
-$_['entry_category']               = 'Category:';
-$_['entry_manufacturer']           = 'Manufacturer:';
-$_['entry_product']                = 'Product:';
-$_['entry_productwise']            = '<span class="help"><b>Product Name:</b><br />(Autocomplete)</span>';
-$_['entry_store']                  = 'Store:';
+$_['entry_category']               = 'Категория:';
+$_['entry_manufacturer']           = 'Производител:';
+$_['entry_product']                = 'Продукт:';
+$_['entry_productwise']            = '<span class="help"><b>Име на продукта:</b><br />(самодовършващо се)</span>';
+$_['entry_store']                  = 'Магазин:';
 $_['entry_layout']                 = 'Layout Override:';
-$_['entry_additional_description'] = 'Additional Description';
+$_['entry_additional_description'] = 'Допълнително описание';
 $_['entry_article_related_method'] = 'Article Related Products:<span class="help">This article is related to a single product, a product category, or a product manufacturer, select accordingly.</span>';
-$_['entry_blog_related_articles']  = 'Related Articles';
+$_['entry_blog_related_articles']  = 'Сродни статии';
 $_['entry_category_wise']          = 'Category Wise';
 $_['entry_manufacturer_wise']      = 'Manufacturer Wise';
 $_['entry_product_wise']           = 'Product Wise';
@@ -57,7 +57,7 @@ $_['error_permission']             = 'Warning: You do not have permission to mod
 $_['error_title']                  = 'Article Title must be greater than 3 and less than 100 characters!';
 $_['error_title_found']            = 'Article Title already exists, article title must be unique!';
 $_['error_description']            = 'Description must be greater than 3 characters!';
-$_['error_author_name']            = 'Author name can not be blank!';
+$_['error_author_name']            = 'Името на автора не може да е празно!';
 $_['error_author_not_found_list']  = 'Author not found in our list!';
 $_['error_author_not_found']       = 'Sorry Author not found in our list, please insert valid author name!';
 $_['error_article_related']        = 'Warning: You can not delete article because currently it relate %s articles!';
