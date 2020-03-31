99ac9fbc684a4ecf8d95ebc6996c66a127b417ea
Philippe Marion <contact@nivocart.com>
2020-03-31 23:01:06 +0100

    New translations author.php (Polish)

--- upload/admin/language/Polish/upload/admin/language/english/blog/author.php
+++ upload/admin/language/Polish/upload/admin/language/english/blog/author.php
@@ -1,30 +1,30 @@
 <?php
 // Heading
-$_['heading_title']          = 'Blog Author';
+$_['heading_title']          = 'Autor bloga';
 
 // Columns
-$_['column_author_name']     = 'Author Name';
+$_['column_author_name']     = 'Nazwa autora';
 $_['column_status']          = 'Status';
-$_['column_date_added']      = 'Date Added';
-$_['column_action']          = 'Action';
+$_['column_date_added']      = 'Data dodania';
+$_['column_action']          = 'Czynność';
 
 // Text
-$_['text_success']           = 'Success: You have modified Blog Authors!';
-$_['text_default']           = 'Default';
-$_['text_image_manager']     = 'File Manager';
-$_['text_browse']            = 'Browse';
-$_['text_clear']             = 'Clear';
+$_['text_success']           = 'Sukces: Zmodyfikowałeś autorów bloga!';
+$_['text_default']           = 'Domyślny';
+$_['text_image_manager']     = 'Menadżer plików';
+$_['text_browse']            = 'Przeglądaj';
+$_['text_clear']             = 'Wyczyść';
 
 // Entry
-$_['entry_name']             = 'Author Name:<span class="help">Author\'s Name must be unique.</span>';
+$_['entry_name']             = 'Nazwa autora:<span class="help">Nazwa autora musi być unikatowa.</span>';
 $_['entry_image']            = 'Avatar:';
 $_['entry_meta_description'] = 'Meta Tag Description:<span class="help">The optimal number of characters is around 155, including spaces.<br />SEO importance: High.</span>';
 $_['entry_meta_keyword']     = 'Meta Tag Keywords:<span class="help">Usually ignored by search engines.<br />SEO importance: Low.</span>';
-$_['entry_description']      = 'Description:';
+$_['entry_description']      = 'Opis:';
 $_['entry_status']           = 'Status:';
 
 // Errors
-$_['error_warning']          = 'Warning: Please check form carefully for errors!';
+$_['error_warning']          = 'Ostrzeżenie: Sprawdź dokładnie formularz pod kątem błędów!';
 $_['error_permission']       = 'Warning: You do not have permission to modify the authors!';
 $_['error_name']             = 'Author Name must be between 3 and 255 characters!';
 $_['error_author_found']     = 'Author Name already exists, author name must unique!';
