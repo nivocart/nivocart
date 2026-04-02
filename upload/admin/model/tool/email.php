6d26441149bfc7751290ca0e7c9088c07807ce76
Philippe Marion <marionphilippe@hotmail.com>
2026-04-02 14:12:14 +0100

    Update email.php
    update model tool email

--- upload/admin/model/tool/email.php
+++ upload/admin/model/tool/email.php
@@ -1,21 +1,23 @@
 <?php
 class ModelToolEmail extends Model {
-
-	public function verifyMail($email) {
-		$valid = false;
-
+	/**
+	 * Check if email string is valid
+	 *
+	 * @return bool
+	 */
+	public function verifyMail(string $email): bool {
 		if ($this->url->isLocal()) {
-			$valid = true;
-		} else {
-			if ($email && filter_var(trim($email), FILTER_VALIDATE_EMAIL)) {
-				$domain = substr(strrchr($email, '@'), 1);
-
-				if (checkdnsrr($domain, 'MX')) {
-					$valid = true;
-				}
-			}
+			return true;
 		}
 
-		return $valid;
+		$email = trim($email);
+
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			return false;
+		}
+
+		$domain = substr(strrchr($email, '@'), 1);
+
+		return checkdnsrr($domain, 'MX');
 	}
 }
