14625b38564c475842eeec9e201964a1445c715a
Philippe Marion <marionphilippe@hotmail.com>
2026-04-20 12:16:52 +0100

    Update extension.php

--- upload/admin/model/setting/extension.php
+++ upload/admin/model/setting/extension.php
@@ -1,10 +1,10 @@
 <?php
 class ModelSettingExtension extends Model {
 
-	public function getInstalled($type): array {
-		$extension_data = array();
+	public function getInstalled(string $type): array {
+		$extension_data = [];
 
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape($type) . "'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape((string)$type) . "'");
 
 		foreach ($query->rows as $result) {
 			$extension_data[] = $result['code'];
@@ -13,22 +13,22 @@ class ModelSettingExtension extends Model {
 		return $extension_data;
 	}
 
-	public function install($type, $code): void {
-		$this->db->query("INSERT INTO `" . DB_PREFIX . "extension` SET `type` = '" . $this->db->escape($type) . "', `code` = '" . $this->db->escape($code) . "'");
+	public function install(string $type, string $code): void {
+		$this->db->query("INSERT INTO `" . DB_PREFIX . "extension` SET `type` = '" . $this->db->escape((string)$type) . "', `code` = '" . $this->db->escape((string)$code) . "'");
 	}
 
-	public function uninstall($type, $code): void {
-		$this->db->query("DELETE FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape($type) . "' AND `code` = '" . $this->db->escape($code) . "'");
+	public function uninstall(string $type, string $code): void {
+		$this->db->query("DELETE FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape((string)$type) . "' AND `code` = '" . $this->db->escape((string)$code) . "'");
 	}
 
-	public function getExtensions($type): array {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape($type) . "' ORDER BY `code`");
+	public function getExtensions(string $type): array {
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape((string)$type) . "' ORDER BY `code`");
 
 		return $query->rows;
 	}
 
-	public function getTotalInstalled($type): int {
-		$query = $this->db->query("SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape($type) . "'");
+	public function getTotalInstalled(string $type): int {
+		$query = $this->db->query("SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "extension` WHERE `type` = '" . $this->db->escape((string)$type) . "'");
 
 		return $query->row['total'];
 	}
