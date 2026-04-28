d1e0398ce9d6f8a83f9bfa7c8cd3b3bcdabd85cc
Philippe Marion <marionphilippe@hotmail.com>
2026-04-28 06:55:59 +0100

    Update session.php

--- upload/system/library/session.php
+++ upload/system/library/session.php
@@ -64,7 +64,7 @@ class Session {
 
 		$this->data = &$_SESSION[$this->session_id];
 
-		if ($key != 'PHPSESSID') {
+		if ($key !== 'PHPSESSID') {
 			setcookie($key, $this->session_id, ini_get('session.cookie_lifetime'), ini_get('session.cookie_path'), ini_get('session.cookie_domain'), ini_get('session.cookie_secure'), ini_get('session.cookie_httponly'));
 		}
 
