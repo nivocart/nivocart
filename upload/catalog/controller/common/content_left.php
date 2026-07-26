655e19d7ee199632f8d12050aac36e156f702c8c
Philippe Marion <nivocart@gmail.com>
2026-07-26 09:32:44 +0100

    Catalog Content Position Controllers update

--- upload/catalog/controller/common/content_left.php
+++ upload/catalog/controller/common/content_left.php
@@ -8,49 +8,53 @@ class ControllerCommonContentLeft extends Controller {
 	/** Error array Placeholder */
 
 	protected function index() {
-		$this->load->model('design/layout');
-		$this->load->model('catalog/category');
-		$this->load->model('catalog/product');
-		$this->load->model('catalog/information');
-		$this->load->model('blog/article');
-
-		if (isset($this->request->get['route'])) {
-			$route = (string)$this->request->get['route'];
-		} else {
-			$route = 'common/home';
-		}
+		$route = isset($this->request->get['route']) ? (string)$this->request->get['route'] : 'common/home';
 
 		$layout_id = 0;
 
-		// Strict validation '===' should not be used on this page, at this time
-		if ($route == 'product/category' && isset($this->request->get['path']) && !is_array($this->request->get['path'])) {
+		if ($route === 'product/category' && isset($this->request->get['path']) && !is_array($this->request->get['path'])) {
 			$path = explode('_', (string)$this->request->get['path']);
 
-			$layout_id = $this->model_catalog_category->getCategoryLayoutId(end($path));
+			$this->load->model('catalog/category');
+
+			$layout_id = (int)$this->model_catalog_category->getCategoryLayoutId(end($path));
 		}
 
-		if ($route == 'product/product' && isset($this->request->get['product_id'])) {
-			$layout_id = $this->model_catalog_product->getProductLayoutId($this->request->get['product_id']);
-		}
+		$route_map = [
+			'product/product'         => ['model' => 'catalog/product', 'key' => 'product_id', 'method' => 'getProductLayoutId'],
+			'information/information' => ['model' => 'catalog/information', 'key' => 'information_id', 'method' => 'getInformationLayoutId'],
+			'blog/article_info'       => ['model' => 'blog/article', 'key' => 'blog_article_id', 'method' => 'getBlogArticleLayoutId'],
+			'blog/category'           => ['model' => 'blog/article', 'key' => 'blog_category_id', 'method' => 'getBlogCategoryLayoutId'],
+		];
 
-		if ($route == 'information/information' && isset($this->request->get['information_id'])) {
-			$layout_id = $this->model_catalog_information->getInformationLayoutId($this->request->get['information_id']);
-		}
+		if (!$layout_id && isset($route_map[$route])) {
+			$entry = $route_map[$route];
 
-		if ($route == 'blog/article_info' && isset($this->request->get['blog_article_id'])) {
-			$layout_id = $this->model_blog_article->getBlogArticleLayoutId($this->request->get['blog_article_id']);
-		}
+			$param_key = $entry['key'];
 
-		if ($route == 'blog/category' && isset($this->request->get['blog_category_id'])) {
-			$layout_id = $this->model_blog_article->getBlogCategoryLayoutId($this->request->get['blog_category_id']);
+			if (isset($this->request->get[$param_key]) && !is_array($this->request->get[$param_key])) {
+				$value = (string)$this->request->get[$param_key];
+
+				if (isset($entry['transform'])) {
+					$value = ($entry['transform'])($value);
+				}
+
+				$this->load->model($entry['model']);
+
+				$model_key = 'model_' . str_replace('/', '_', $entry['model']);
+
+				$layout_id = (int)$this->{$model_key}->{$entry['method']}($value);
+			}
 		}
 
 		if (!$layout_id) {
-			$layout_id = $this->model_design_layout->getLayout($route);
+			$this->load->model('design/layout');
+
+			$layout_id = (int)$this->model_design_layout->getLayout($route);
 		}
 
 		if (!$layout_id) {
-			$layout_id = $this->config->get('config_layout_id');
+			$layout_id = (int)$this->config->get('config_layout_id');
 		}
 
 		$module_data = [];
@@ -64,7 +68,7 @@ class ControllerCommonContentLeft extends Controller {
 
 			if ($modules) {
 				foreach ($modules as $module) {
-					if ($module['layout_id'] == $layout_id && $module['position'] == 'content_left' && $module['status']) {
+					if ($module['layout_id'] == $layout_id && $module['position'] === 'content_left' && $module['status']) {
 						$module_data[] = [
 							'code'       => $extension['code'],
 							'setting'    => $module,
@@ -75,21 +79,15 @@ class ControllerCommonContentLeft extends Controller {
 			}
 		}
 
-		$sort_order = [];
-
-		foreach ($module_data as $key => $value) {
-			$sort_order[$key] = $value['sort_order'];
-		}
-
-		array_multisort($sort_order, SORT_ASC, $module_data);
+		usort($module_data, fn($a, $b) => $a['sort_order'] <=> $b['sort_order']);
 
 		$this->data['modules'] = [];
 
 		foreach ($module_data as $module) {
-			$module = $this->getChild('module/' . $module['code'], $module['setting']);
+			$rendered = $this->getChild('module/' . $module['code'], $module['setting']);
 
-			if ($module) {
-				$this->data['modules'][] = $module;
+			if ($rendered) {
+				$this->data['modules'][] = $rendered;
 			}
 		}
 
