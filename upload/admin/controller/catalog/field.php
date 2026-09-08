13921e1f93ffbbfaff5c644c3447a4cfe691d625
Philippe Marion <nivocart@gmail.com>
2026-09-08 11:55:00 +0100

    Admin Catalog Controllers update

--- upload/admin/controller/catalog/field.php
+++ upload/admin/controller/catalog/field.php
@@ -484,7 +484,7 @@ class ControllerCatalogField extends Controller {
 		foreach ($this->request->post['selected'] as $field_id) {
 			$product_total = $this->model_catalog_product->getTotalProductsByFieldId($field_id);
 
-			if ($product_total !== null) {
+			if ($product_total > 0) {
 				$this->error['warning'] = sprintf($this->language->get('error_product'), $product_total);
 			}
 		}
