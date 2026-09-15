2b9610d2422d019751a311ae5868d2ee03a189af
Philippe Marion <nivocart@gmail.com>
2026-09-15 10:32:59 +0100

    Update success.php

--- upload/catalog/language/english/checkout/success.php
+++ upload/catalog/language/english/checkout/success.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'Your Order Has Been Processed!';
+$_['heading_title'] = 'Your Order Has Been Received!';
 
 // Text
 $_['text_customer'] = '<p>Your order has been successfully submitted!</p><p>You can view your order history by going to the <a href="%s">my account</a> page and by clicking on <a href="%s">history</a>.</p><p>If your purchase has an associated download, you can go to the account <a href="%s">downloads</a> page to view them.</p><p>Please direct any questions you have to our store\'s <a href="%s">customer service</a>.</p><p>Thank you for shopping with us!</p>';
