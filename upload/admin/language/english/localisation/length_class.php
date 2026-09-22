b24a180573846b40adb6a6993c2f59d3c5e7e8cd
Philippe Marion <nivocart@gmail.com>
2026-09-22 07:43:22 +0100

    Admin Language files update

--- upload/admin/language/english/localisation/length_class.php
+++ upload/admin/language/english/localisation/length_class.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']    = 'Length Class';
+$_['heading_title']        = 'Length Class';
 
 // Text
-$_['text_success']          = 'Success: You have modified <b>Length Classes</b> !';
-$_['text_default_success']  = 'Success: The default length class has been updated!';
+$_['text_success']         = 'Success: You have modified <b>Length Classes</b> !';
+$_['text_default_success'] = 'Success: The default length class has been updated!';
 
 // Column
-$_['column_title']     = 'Length Title';
-$_['column_unit']      = 'Length Unit';
-$_['column_value']     = 'Value';
-$_['column_action']    = 'Action';
+$_['column_title']         = 'Length Title';
+$_['column_unit']          = 'Length Unit';
+$_['column_value']         = 'Value';
+$_['column_action']        = 'Action';
 
 // Entry
-$_['entry_title']      = 'Length Title:';
-$_['entry_unit']       = 'Length Unit:';
-$_['entry_value']           = 'Value:<span class="help">Conversion multiplier relative to the base unit (the unit with value 1.00000). Example: if cm = 1.00000, then mm = 0.10000, inch = 2.54000, m = 100.00000. To change the default length unit for new products, use the <b>Set Default</b> action on the list page.</span>';
+$_['entry_title']          = 'Length Title:';
+$_['entry_unit']           = 'Length Unit:';
+$_['entry_value']          = 'Value:<span class="help">Conversion multiplier relative to the base unit (the unit with value 1.00000). Example: if cm = 1.00000, then mm = 0.10000, inch = 2.54000, m = 100.00000. To change the default length unit for new products, use the <b>Set Default</b> action on the list page.</span>';
 
 // Button
-$_['button_set_default']    = 'Set Default';
+$_['button_set_default']   = 'Set Default';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify <b>Length Classes</b> !';
-$_['error_title']      = 'Length Title must be between 3 and 32 characters!';
-$_['error_unit']       = 'Length Unit must be between 1 and 4 characters!';
-$_['error_default']    = 'Warning: This length class cannot be deleted as it is currently assigned as the default store length class!';
-$_['error_product']    = 'Warning: This length class cannot be deleted as it is currently assigned to %s products!';
+$_['error_permission']     = 'Warning: You do not have permission to modify <b>Length Classes</b> !';
+$_['error_title']          = 'Length Title must be between 3 and 32 characters!';
+$_['error_unit']           = 'Length Unit must be between 1 and 4 characters!';
+$_['error_default']        = 'Warning: This length class cannot be deleted as it is currently assigned as the default store length class!';
+$_['error_product']        = 'Warning: This length class cannot be deleted as it is currently assigned to %s products!';
--- upload/admin/language/english/localisation/weight_class.php
+++ upload/admin/language/english/localisation/weight_class.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']    = 'Weight Class';
+$_['heading_title']        = 'Weight Class';
 
 // Text
-$_['text_success']          = 'Success: You have modified <b>Weight Classes</b> !';
-$_['text_default_success']  = 'Success: The default weight class has been updated!';
+$_['text_success']         = 'Success: You have modified <b>Weight Classes</b> !';
+$_['text_default_success'] = 'Success: The default weight class has been updated!';
 
 // Column
-$_['column_title']     = 'Weight Title';
-$_['column_unit']      = 'Weight Unit';
-$_['column_value']     = 'Value';
-$_['column_action']    = 'Action';
+$_['column_title']         = 'Weight Title';
+$_['column_unit']          = 'Weight Unit';
+$_['column_value']         = 'Value';
+$_['column_action']        = 'Action';
 
 // Entry
-$_['entry_title']      = 'Weight Title:';
-$_['entry_unit']       = 'Weight Unit:';
-$_['entry_value']           = 'Value:<span class="help">Conversion multiplier relative to the base unit (the unit with value 1.00000). Example: if kg = 1.00000, then g = 0.00100, lb = 0.45359, oz = 0.02835. To change the default weight unit for new products, use the <b>Set Default</b> action on the list page.</span>';
+$_['entry_title']          = 'Weight Title:';
+$_['entry_unit']           = 'Weight Unit:';
+$_['entry_value']          = 'Value:<span class="help">Conversion multiplier relative to the base unit (the unit with value 1.00000). Example: if kg = 1.00000, then g = 0.00100, lb = 0.45359, oz = 0.02835. To change the default weight unit for new products, use the <b>Set Default</b> action on the list page.</span>';
 
 // Button
-$_['button_set_default']    = 'Set Default';
+$_['button_set_default']   = 'Set Default';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify <b>Weight Classes</b> !';
-$_['error_title']      = 'Weight Title must be between 3 and 32 characters!';
-$_['error_unit']       = 'Weight Unit must be between 1 and 4 characters!';
-$_['error_default']    = 'Warning: This weight class cannot be deleted as it is currently assigned as the default store weight class!';
-$_['error_product']    = 'Warning: This weight class cannot be deleted as it is currently assigned to %s products!';
+$_['error_permission']     = 'Warning: You do not have permission to modify <b>Weight Classes</b> !';
+$_['error_title']          = 'Weight Title must be between 3 and 32 characters!';
+$_['error_unit']           = 'Weight Unit must be between 1 and 4 characters!';
+$_['error_default']        = 'Warning: This weight class cannot be deleted as it is currently assigned as the default store weight class!';
+$_['error_product']        = 'Warning: This weight class cannot be deleted as it is currently assigned to %s products!';
